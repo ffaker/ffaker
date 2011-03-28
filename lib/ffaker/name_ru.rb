a1472049b909f58209a012547de608c4c800231a
Vsevolod Romashov <7@7vn.ru>
2011-03-29 00:51:54 +0400

    added sex differentiation to names

--- lib/ffaker/name_ru.rb
+++ lib/ffaker/name_ru.rb
@@ -7,27 +7,41 @@ module Faker
     
     def name
       case rand(2)
+      when 0 then @sex = :male
+      when 1 then @sex = :female
+      end
+      
+      case rand(2)
       when 0 then "#{last_name} #{first_name} #{patronymic}"
       else        "#{first_name} #{last_name}"
       end
     end
     
-    def first_name
-      FIRST_NAMES.rand
+    def last_name
+      LAST_NAMES[@sex].rand
     end
     
-    def last_name
-      LAST_NAMES.rand
+    def first_name
+      FIRST_NAMES[@sex].rand
     end
     
     def patronymic
-      PATRONYMICS.rand
+      PATRONYMICS[@sex].rand
     end
     
-    FIRST_NAMES = k %w(Александр Алексей Василий Иван Максим Павел)
+    LAST_NAMES = {
+      :male   => k(%w(Петров Иванов Козлодоев Пупкин)),
+      :female => k(%w(Петрова Иванова Козлодоева Пупкина))
+    }
     
-    LAST_NAMES = k %w(Петров Иванов Козлодоев Пупкин)
+    FIRST_NAMES = {
+      :male   => k(%w(Александр Алексей Василий Иван Максим Павел)),
+      :female => k(%w(Александра Василиса Елена Ольга Наталья))
+    }
     
-    PATRONYMICS = k %w(Антонович Александрович Алексеевич Васильевич Иванович Максимович Павлович)
+    PATRONYMICS = {
+      :male   => k(%w(Антонович Александрович Алексеевич Васильевич Иванович Максимович Павлович)),
+      :female => k(%w(Антоновна Александровна Алексеевна Васильевна Ивановна Максимовна Павловна))
+    }
   end
 end
--- test/test_faker_name_ru.rb
+++ test/test_faker_name_ru.rb
@@ -3,12 +3,26 @@ require 'helper'
 
 class TestFakerNameRu < Test::Unit::TestCase
   def setup
-    @tester = Faker::NameRU
+    @words = Faker::NameRU.name.split
   end
   
   def test_name
-    words = @tester.name.split
-    assert [2,3].include?(words.size)
-    assert words.each { |word| word.match /[А-Я][а-я]+/ }
+    assert [2,3].include?(@words.size)
+    assert @words.each { |word| word.match /[А-Я][а-я]+/ }
+  end
+  
+  def test_sex
+    assert same_sex?(@words)
+  end
+  
+  # checks if every name is of the same sex
+  def same_sex?(words)
+    [:male, :female].any? do |sex|
+      words.all? do |word|
+        [Faker::NameRU::LAST_NAMES, Faker::NameRU::FIRST_NAMES, Faker::NameRU::PATRONYMICS].any? do |names|
+          names[sex].include?(word)
+        end
+      end
+    end
   end
 end
