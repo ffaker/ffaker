13bcff7e7875c1abe1b46d44781246d33544ae6f
Emmanuel Oga <EmmanuelOga@gmail.com>
2011-04-02 14:49:27 -0300

    removing with_same_sex

--- test/test_faker_name_ru.rb
+++ test/test_faker_name_ru.rb
@@ -4,67 +4,66 @@ require 'helper'
 class TestFakerNameRu < Test::Unit::TestCase
   def setup
     @tester = Faker::NameRU
-    @words = @tester.name.split
   end
-  
+
   def test_name
+    @words = @tester.name.split
     assert [2,3].include?(@words.size)
     assert @words.each { |word| word.match /[А-Я][а-я]+/ }
   end
-  
+
   def test_name_sex
+    @words = @tester.name.split
     assert same_sex?(@words)
   end
-  
+
   def test_uniqueness
     unique_names = (1..10000).map { @tester.name }.uniq.size
     assert unique_names > 9850, "got only #{unique_names} unique names out of 10000"
   end
-  
+
   def test_last_name
     assert @tester.last_name.match(/[А-Я][а-я]+/)
   end
-  
+
   def test_male_last_name
     assert Faker::NameRU::LAST_NAMES[:male].include?(@tester.last_name(:male))
   end
-  
+
   def test_first_name
     assert @tester.first_name.match(/[А-Я][а-я]+/)
   end
-  
+
   def test_male_first_name
     assert Faker::NameRU::FIRST_NAMES[:male].include?(@tester.first_name(:male))
   end
-  
+
   def test_patronymic
     assert @tester.patronymic.match(/[А-Я][а-я]+/)
   end
-  
+
   def test_male_patronymic
     assert Faker::NameRU::PATRONYMICS[:male].include?(@tester.patronymic(:male))
   end
-  
+
   def test_with_same_sex
+    sex = [:male, :female][rand(2)]
     names = []
-    @tester.with_same_sex do
-      names << @tester.last_name
-      names << @tester.first_name
-      names << @tester.patronymic
-    end
+    names << @tester.last_name(sex)
+    names << @tester.first_name(sex)
+    names << @tester.patronymic(sex)
     assert same_sex?(names)
   end
-  
+
   def test_with_same_sex_for_male
+    sex = :male
     names = []
-    @tester.with_same_sex(:male) do
-      names << @tester.last_name
-      names << @tester.first_name
-      names << @tester.patronymic
-    end
+    names << @tester.last_name(sex)
+    names << @tester.first_name(sex)
+    names << @tester.patronymic(sex)
     assert same_sex?(names, :male)
   end
-  
+
   # checks if every name is of the same sex
   def same_sex?(words, sex = :any)
     (sex == :any ? [:male, :female] : [sex]).any? do |sex|
