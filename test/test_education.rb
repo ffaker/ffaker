a2bff9d77503a9cf61eb274725b12e5683b2826c
Emmanuel Oga <EmmanuelOga@gmail.com>
2012-02-27 19:13:06 -0300

    adding missing test

---
+++ test/test_education.rb
@@ -0,0 +1,32 @@
+require 'helper'
+
+class TestFakerEducation < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Education
+  end
+
+  def test_degree_short
+    assert @tester.degree_short.match(/.+ in .+/)
+  end
+
+  def test_degree
+    assert @tester.degree.match(/.+ in .+/)
+  end
+
+  def test_major
+    assert @tester.major.match(/.+ .+/)
+  end
+
+  def test_school_name
+    assert @tester.school_name.match(/.+/)
+  end
+
+  def test_school_generic_name
+    assert @tester.school_name.match(/.+/)
+  end
+
+  def test_school
+    assert @tester.school.match(/.+ .+( .+)?/)
+  end
+end
+
