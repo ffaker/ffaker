6b8cc08f02fa64acf01d539b50823a40a8654675
Kristian Mandrup <kmandrup@gmail.com>
2012-03-27 13:20:20 +0200

    added danish addresses

---
+++ lib/ffaker/address_da.rb
@@ -0,0 +1,39 @@
+module Faker
+  module AddressDA
+    include Faker::Address
+    extend ModuleUtils
+    extend self
+
+    def zip_code
+      Faker.numerify ZIP_FORMATS.rand
+    end
+
+    def state
+      STATE.rand
+    end
+
+    def city
+      CITY.rand
+    end
+
+    ZIP_FORMATS = k ['#####']
+
+    def street_name
+      STREET.rand
+    end
+
+    STREET = k [
+      'maglekildevej', 'griffenfeldsgade', 'frederiksberg alle', 'kgs. nytorv', 'øster alle', 'lyngbyvej',
+      'pile alle', 'gothersgade'
+    ]
+
+    STATE = k [
+      'Nord Jylland', 'Vest Jylland', 'Øst Jylland', 'Syd Jylland', 'Fyn',
+      'Lolland Falster', 'København'
+    ]
+
+    CITY = k %w(
+      Århus Ålborg Helsingør København Kolding Middelfart Ribe Randers Slagelse Skagen
+    )
+  end
+end
