a21f392569ce725e63547bfbbf8af352010de8d5
Andrew Davis <andrew.r.davis@nasa.gov>
2012-11-09 16:34:33 -0500

    Added Identification and Time helpers

---
+++ test/test_identification.rb
@@ -0,0 +1,25 @@
+require 'helper'
+
+class TestFakerIdentification < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Identification
+  end
+
+  def test_drivers_license
+    ###-###-##-###-#
+    assert_match /[A-Z]\d{3}-\d{3}-\d{2}-\d{3}-\d{1}/, @tester.drivers_license
+  end
+
+  def test_ssn
+    assert_match /\d{3}-\d{2}-\d{4}/, @tester.ssn
+  end
+
+  def test_gender
+    assert_match /(Male|Female)/, @tester.gender
+  end
+
+  def test_ethnicity
+    ethnicity_regex = /(African American|Asian\/Pacific Islander|Caucasian|Hispanic|Native American|Multiracial|Other|Prefer not to respond)/
+    assert_match ethnicity_regex, @tester.ethnicity
+  end
+end
