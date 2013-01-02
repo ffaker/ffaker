825bae1fca7e0736161f1554eed3168cefb73abc
sanemat <o.gata.ken@gmail.com>
2013-01-03 08:13:07 +0900

    Remove deprecate, Use AddressUS module
    [zip_code] is deprecated. For US addresses please use the AddressUS
    module

--- test/test_address.rb
+++ test/test_address.rb
@@ -42,19 +42,19 @@ class TestAddress < Test::Unit::TestCase
   end
 
   def test_us_state
-    assert_match /[ a-z]/, Faker::Address.us_state
+    assert_match /[ a-z]/, Faker::AddressUS.state
   end
 
   def test_us_state_abbr
-    assert_match /[A-Z]/, Faker::Address.us_state_abbr
+    assert_match /[A-Z]/, Faker::AddressUS.state_abbr
   end
 
   def test_zip_code
-    assert_match /[0-9]/, Faker::Address.zip_code
+    assert_match /[0-9]/, Faker::AddressUS.zip_code
   end
 
   def test_zip_code_frozen
-    assert Faker::Address.zip_code.frozen? == false
+    assert Faker::AddressUS.zip_code.frozen? == false
   end
 
   def test_neighborhood
