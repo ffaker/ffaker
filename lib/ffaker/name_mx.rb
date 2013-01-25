a6ce8a0564e096f4a49e5a42c78b196e0ef05f88
Pablo Ruiz <pjruiz@maquech.com.mx>
2013-01-25 13:24:40 -0600

    updated method names for better understandability

--- lib/ffaker/name_mx.rb
+++ lib/ffaker/name_mx.rb
@@ -107,41 +107,48 @@ module Faker
     MALE_PREFIXES = k %w(Sr. C.)
     FEMALE_PREFIXES = k %w(Sra. Srita. C.)
     PREFIXES = k %w(Sr. Sra. Srita. C.)
-
-    # Full name according to gender, possibly with two names, possible with prefix
-    def name(gender = :any)
+    
+    # Full name according to gender and prefix, possibly with middle_name
+    def full_name(gender = :any, prefix = false)
+      if prefix
+        full_name_prefix(gender)
+      else
+        full_name_no_prefix(gender)
+      end
+    end
+    
+    # Full name with prefix according to gender, possibly with middle_name
+    def full_name_prefix(gender = :any)
       case gender
         when :any then
           case rand(9)
-            when 0, 3, 6, 8 then prefix_female_name
-            else prefix_male_name
+            when 0, 3, 6, 8 then "#{female_prefix} #{female_name} #{paternal_last_names}"
+            else "#{male_prefix} #{male_name} #{paternal_last_names}"
           end
-        when :male then prefix_male_name
-        when :female then prefix_female_name
+        when :male then "#{male_prefix} #{male_name} #{paternal_last_names}"
+        when :female then "#{female_prefix} #{female_name} #{paternal_last_names}"
         else raise ArgumentError, "Invalid gender, must be one of :any, :male, :female"
       end
     end
     
-    # Full male name possibly with prefix
-    def prefix_male_name
-      case rand(9)
-        when 0, 5 then "#{male_prefix} #{male_name} #{paternal_last_names}"
-        else "#{male_name} #{paternal_last_names}"
-      end
-    end
-    
-    # Full female name possibly with prefix
-    def prefix_female_name
-      case rand(9)
-        when 0, 5 then "#{female_prefix} #{female_name} #{paternal_last_names}"
-        else"#{female_name} #{paternal_last_names}"
+    # Full name with no prefix according to gender, possibly with middle_name
+    def full_name_no_prefix(gender = :any)
+      case gender
+        when :any then
+          case rand(9)
+            when 0, 3, 6, 8 then "#{female_name} #{paternal_last_names}"
+            else "#{male_name} #{paternal_last_names}"
+          end
+        when :male then "#{male_name} #{paternal_last_names}"
+        when :female then "#{female_name} #{paternal_last_names}"
+        else raise ArgumentError, "Invalid gender, must be one of :any, :male, :female"
       end
     end
    
     # Male first name and possibly middle name
     def male_name
       case rand(9)
-        when 0, 5 then "#{first_name(:male)} #{first_name(:male)}"
+        when 0, 5 then "#{first_name(:male)} #{middle_name(:male)}"
         else first_name(:male)
       end
     end
@@ -149,13 +156,13 @@ module Faker
     # Female first name and possibly middle name
     def female_name
       case rand(9)
-        when 0, 5 then "#{first_name(:female)} #{first_name(:female)}"
+        when 0, 5 then "#{first_name(:female)} #{middle_name(:female)}"
         else first_name(:female)
       end
     end
 
-    # Single name according to gender parameter
-    def first_name(gender = :any)
+    # A single name according to gender parameter
+    def name(gender = :any)
       case gender
         when :any then FIRST_NAMES.rand
         when :male then MALE_FIRST_NAMES.rand
@@ -163,6 +170,8 @@ module Faker
         else raise ArgumentError, "Invalid gender, must be one of :any, :male, :female"
       end
     end
+    alias_method :middle_name, :name
+    alias_method :first_name, :name
     
     # Father's and mother's last name
     def paternal_last_names
--- test/test_faker_name_mx.rb
+++ test/test_faker_name_mx.rb
@@ -10,6 +10,30 @@ class TestFakerNameMX < Test::Unit::TestCase
   def test_last_name
     assert Faker::NameMX::LAST_NAMES.include?(@tester.last_name)
   end
+  
+  def test_first_name
+    assert Faker::NameMX::FIRST_NAMES.include?(@tester.first_name)
+  end
+  
+  def test_middle_name
+    assert Faker::NameMX::FIRST_NAMES.include?(@tester.middle_name)
+  end
+  
+  def test_name
+    assert Faker::NameMX::FIRST_NAMES.include?(@tester.name)
+  end
+
+  def test_prefix_male
+    assert Faker::NameMX::MALE_PREFIXES.include?(@tester.male_prefix)
+  end
+
+  def test_prefix_female
+    assert Faker::NameMX::FEMALE_PREFIXES.include?(@tester.female_prefix)
+  end
+  
+  def test_prefix
+    assert Faker::NameMX::PREFIXES.include?(@tester.prefix)
+  end
 
   def test_male_name
     parts = @tester.male_name.split(' ')
@@ -28,25 +52,9 @@ class TestFakerNameMX < Test::Unit::TestCase
         Faker::NameMX::FEMALE_FIRST_NAMES.include?(parts[1])
     end
   end
-  
-  def test_first_name
-    assert Faker::NameMX::FIRST_NAMES.include?(@tester.first_name)
-  end
 
-  def test_prefix_male
-    assert Faker::NameMX::MALE_PREFIXES.include?(@tester.male_prefix)
-  end
-
-  def test_prefix_female
-    assert Faker::NameMX::FEMALE_PREFIXES.include?(@tester.female_prefix)
-  end
-  
-  def test_prefix
-    assert Faker::NameMX::PREFIXES.include?(@tester.prefix)
-  end
-
-  def test_name_male
-    parts = @tester.name(:male).split(' ')
+  def test_full_name_male
+    parts = @tester.full_name(:male).split(' ')
     case parts.count
     when 5
       assert Faker::NameMX::MALE_PREFIXES.include?(parts[0])
@@ -71,8 +79,8 @@ class TestFakerNameMX < Test::Unit::TestCase
     end
   end
 
-  def test_name_female
-    parts = @tester.name(:female).split(' ')
+  def test_full_name_female
+    parts = @tester.full_name(:female).split(' ')
     case parts.count
     when 5
       assert Faker::NameMX::FEMALE_PREFIXES.include?(parts[0])
@@ -97,8 +105,51 @@ class TestFakerNameMX < Test::Unit::TestCase
     end
   end
 
-  def test_name
-    parts = @tester.name.split(' ')
+  def test_full_name
+    parts = @tester.full_name.split(' ')
+    case parts.count
+    when 5
+      assert Faker::NameMX::PREFIXES.include?(parts[0])
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[1])
+      # Middle name
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[2])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[3])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[4])
+    when 4
+      prefix_or_first_name = Faker::NameMX::PREFIXES.include?(parts[0]) ||
+        Faker::NameMX::FIRST_NAMES.include?(parts[0])
+      assert prefix_or_first_name
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[1])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[2])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[3])
+    when 3
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[0])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[1])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[2])
+    else
+      flunk "Invalid Name"
+    end
+  end
+  
+  def test_full_name_no_prefix
+    parts = @tester.full_name_no_prefix.split(' ')
+    case parts.count
+    when 4
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[0])
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[1])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[2])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[3])
+    when 3
+      assert Faker::NameMX::FIRST_NAMES.include?(parts[0])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[1])
+      assert Faker::NameMX::LAST_NAMES.include?(parts[2])
+    else
+      flunk "Invalid Name"
+    end
+  end
+  
+  def test_full_name_prefix
+    parts = @tester.full_name_prefix.split(' ')
     case parts.count
     when 5
       assert Faker::NameMX::PREFIXES.include?(parts[0])
