6bb90fe3c231829f3afc89d12fafa1c00271c37d
Alter Lagos <alter.strife@gmail.com>
2013-08-22 19:35:41 -0400

    Added RUT (Chilean Id Code) generator

---
+++ test/test_identification_es.rb
@@ -0,0 +1,12 @@
+require 'helper'
+
+class TestFakerIdentificationES < Test::Unit::TestCase
+  def setup
+    @tester = Faker::IdentificationES
+  end
+
+  def test_gender
+    assert_match /(Hombre|Mujer)/, @tester.gender
+  end
+
+end
---
+++ test/test_identification_es_cl.rb
@@ -0,0 +1,12 @@
+require 'helper'
+
+class TestFakerIdentificationESCL < Test::Unit::TestCase
+  def setup
+    @tester = Faker::IdentificationESCL
+  end
+
+  def test_rut
+    assert_match /\b\d{7,8}\-[k|0-9]/i, @tester.rut
+  end
+
+end
