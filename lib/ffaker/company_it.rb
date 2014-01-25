a14ed538b22700b98d80b4048d40a4a10ce165be
Daniele Spinosa <info@microspino.com>
2014-01-25 15:26:10 +0100

    italian company name gen. with tests

---
+++ lib/ffaker/company_it.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+module Faker
+  module CompanyIT
+    extend ModuleUtils
+    extend self
+
+    def name
+      case rand(2)
+      when 0 then "#{NameIT.last_name} #{suffix}"
+      when 1 then "#{NameIT.last_name} e #{NameIT.last_name} #{suffix}"
+      when 2 then "#{prefix} #{NameIT.last_name}"
+      end
+    end
+
+    def suffix
+      SUFFIXES.rand
+    end
+    
+    def prefix
+      PREFIXES.sample
+    end
+    
+    PREFIXES = k ["Studio Legale", "Studio Tecnico", "Laboratorio"]
+    SUFFIXES = k %w(S.p.a. s.r.l. s.n.c. Avvocati Architetti Ingegneri)
+
+  end
+end
