8988affc44f597a86161477a8d52bfa01d1940a5
Moritz Lawitschka <moritz@urbanvention.com>
2014-03-24 10:09:36 +0100

    Locale names and codes

---
+++ lib/ffaker/locale.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+module Faker
+  module Locale
+    extend ModuleUtils
+    extend self
+
+    def code(given_language = nil)
+      code_index = LANGUAGE.index(given_language)
+      if given_language && code_index
+        LANGUAGE_CODE[code_index]
+      else
+        LANGUAGE_CODE.rand
+      end
+    end
+
+    def language(given_code = nil)
+      language_index = LANGUAGE_CODE.index(given_code.upcase)
+      if given_code && language_index
+        LANGUAGE[language_index]
+      else
+        LANGUAGE.rand
+      end
+    end
+
+  end
+end
