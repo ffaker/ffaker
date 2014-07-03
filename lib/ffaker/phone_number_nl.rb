da6e1636a81bca02a2ee2d97d28786435fdbe73f
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-07-03 13:24:49 +0200

    add a class for Dutch phone numbers
    This one might be a bit too realistic. I borrowed the logic from the
    French phone numbers.

---
+++ lib/ffaker/phone_number_nl.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+module Faker
+  module PhoneNumberNL
+    extend ModuleUtils
+    extend self
+
+    def phone_number
+      case rand(2)
+      when 0 then home_work_phone_number
+      when 1 then mobile_phone_number
+      end
+    end
+
+    def home_work_phone_number
+      area = AREA_PREFIX.rand
+      Faker.numerify "0#{area}-#{PHONE_NUMBER[area.size].rand}"
+    end
+
+    def mobile_phone_number
+      Faker.numerify "06#{MOBILE_PHONE_NUMBER.rand}"
+    end
+
+    PHONE_NUMBER = {
+      2 => k(["#######", "### ####", "#### ###"]),
+      3 => k(["######", "#### ##", "## ## ##"])
+    }
+
+    AREA_PREFIX = k %w(10 111 113 114 115 117 118 13 14 15 161 162 164 165 166 167 168 172 174 180 181 182 183 184 186 187 20 222 223 224 226 227 228 229 23 24 251 252 255 26 294 297 299 30 313 314 315 316 317 318 320 321 33 341 342 343 344 345 346 347 348 35 36 38 40 411 412 413 416 418 43 45 46 475 478 481 485 486 487 488 492 493 495 497 499 50 511 512 513 514 515 516 517 518 519 521 522 523 524 525 527 528 529 53 541 543 544 545 546 547 548 55 561 562 566 570 571 572 573 575 577 578 58 591 592 593 594 595 596 597 598 599 70 71 72 73 74 75 76 77 78 79)
+    MOBILE_PHONE_NUMBER = k ['########', ' ## ## ## ##', ' ### ### ##', " #### ####"]
+  end
+end
+
