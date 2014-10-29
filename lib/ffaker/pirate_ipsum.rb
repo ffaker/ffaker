d792ff5da59beb261b557aa4b3833d9e3ac46d61
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-29 16:35:44 -0400

    Add Pirate Ipsum

---
+++ lib/ffaker/pirate_ipsum.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'ffaker/lorem'
+
+module Faker
+  module PirateIpsum
+    include Lorem
+
+    extend ModuleUtils
+    extend self
+
+    def word
+      PIRATE_WORDS.rand
+    end
+
+    def words(num = 3)
+      PIRATE_WORDS.random_pick(num)
+    end
+  end
+end
+
