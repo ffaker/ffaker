417e240dc0b57c93a45e08d361bfccafd1d2b85c
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-29 19:16:19 -0400

    update all ipsum to have sentences and paragraph

--- lib/ffaker/bacon_ipsum.rb
+++ lib/ffaker/bacon_ipsum.rb
@@ -17,5 +17,28 @@ module Faker
     def words(num = 3)
       BACON_WORDS.random_pick(num)
     end
+
+    def sentence(word_count = 4)
+      s = words(word_count + rand(5) + 1)
+      s[0].capitalize!
+      s = s.join(' ')
+      "#{s}."
+    end
+
+    alias_method :phrase, :sentence
+
+    def sentences(sentence_count = 3)
+      (1..sentence_count).map { sentence }
+    end
+
+    alias_method :phrases, :sentences
+
+    def paragraph(sentence_count = 3)
+      sentences(sentence_count + rand(3)).join(' ')
+    end
+
+    def paragraphs(paragraph_count = 3)
+      (1..paragraph_count).map { paragraph }
+    end
   end
 end
