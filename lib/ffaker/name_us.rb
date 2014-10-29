304e4c9463bf31f37ad5ce8d72dc34e826bd97e2
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-29 19:24:29 -0400

    adding US specific first names for genders

---
+++ lib/ffaker/name_us.rb
@@ -0,0 +1,63 @@
+require 'ffaker'
+require 'ffaker/name'
+require 'ffaker/utils/module_utils'
+
+module Faker
+    module NameUS
+        include Faker::Name
+
+        extend ModuleUtils
+        extend self
+
+        def name gender = :any
+            case gender
+            when :any then any_name
+            when :male then male_name
+            when :female then female_name
+            else
+                raise ArgumentError, "Invalid gender, must be one of :any, :male, :female"
+            end
+        end
+
+        def any_name
+            case rand(8)
+            when 0    then "#{prefix} #{first_name} #{last_name}"
+            when 1..2 then "#{first_name} #{last_name} #{last_name}"
+            else           "#{first_name} #{last_name}"
+            end
+        end
+
+        def male_name
+            fname = first_name :male
+            case rand(8)
+            when 0    then "#{prefix} #{fname} #{last_name}"
+            when 1..2 then "#{fname} #{last_name} #{last_name}"
+            else           "#{fname} #{last_name}"
+            end
+        end
+
+        def female_name
+            fname = first_name :female
+            case rand(8)
+            when 0    then "#{prefix} #{fname} #{last_name}"
+            when 1..2 then "#{fname} #{last_name} #{last_name}"
+            else           "#{fname} #{last_name}"
+            end
+        end
+
+        def first_name gender = :any
+            case gender
+            when :any then (rand(2) == 0) ? name(:male) : name(:female)
+            when :male then MALE_FIRST_NAMES.rand
+            when :female then FEMALE_FIRST_NAMES.rand
+            else
+                raise ArgumentError, "Invalid gender, must be one of :any, :male, :female"
+            end
+        end
+
+        def last_name
+            LAST_NAMES.rand
+        end
+    end
+end
+
