30f432dbbb1655d6645a387d964b3b196c43441a
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-10-30 14:26:23 -0400

    add hillbilly ipsum

---
+++ lib/ffaker/hillbilly_ipsum.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+require 'ffaker/lorem'
+
+module Faker
+  # thanks to http://andymatthews.net/code/fillertext/hillbilly.cfm
+  module HillbillyIpsum
+    include Lorem
+
+    extend ModuleUtils
+    extend self
+
+    def word
+      HILLBILLY_WORDS.rand
+    end
+
+    def words(num = 3)
+      HILLBILLY_WORDS.random_pick(num)
+    end
+
+    def sentence(word_count = 4)
+      s = words(word_count + rand(5) + 1)
+      s[0].capitalize!
+      s = s.join(' ')
+      "#{s}."
+    end
+
+    alias_method :phrase, :sentence
+
+    def sentences(sentence_count = 3)
+      (1..sentence_count).map { sentence }
+    end
+
+    alias_method :phrases, :sentences
+
+    def paragraph(sentence_count = 3)
+      sentences(sentence_count + rand(3)).join(' ')
+    end
+
+    def paragraphs(paragraph_count = 3)
+      (1..paragraph_count).map { paragraph }
+    end
+  end
+end
