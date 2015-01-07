cf3c6c750b4255c585b459fcb690f3e6d3cf8993
Hannes Maack <hmaack@rocknruby.it>
2015-01-07 21:36:23 +0100

    fixed the "+1" hipster ipsum unit test regex

--- test/test_hipster_ipsum.rb
+++ test/test_hipster_ipsum.rb
@@ -3,27 +3,29 @@
 require 'helper'
 
 class TestHipsterIpsum < Test::Unit::TestCase
+  TEST_REGEX = /\+1|[ a-z]+/i
+
   def test_paragraph
-    assert_match /1\+|[ a-z]+/i, Faker::HipsterIpsum.paragraph
+    assert_match TEST_REGEX, Faker::HipsterIpsum.paragraph
   end
 
   def test_sentence
-    assert_match /1\+|[ a-z]+/i, Faker::HipsterIpsum.sentence
+    assert_match TEST_REGEX, Faker::HipsterIpsum.sentence
   end
 
   def test_paragraphs
-    assert_match /1\+|[ a-z]+/i, Faker::HipsterIpsum.paragraphs.join(" ")
+    assert_match TEST_REGEX, Faker::HipsterIpsum.paragraphs.join(" ")
   end
 
   def test_sentences
-    assert_match /1\+|[ a-z]+/i, Faker::HipsterIpsum.sentences.join(" ")
+    assert_match TEST_REGEX, Faker::HipsterIpsum.sentences.join(" ")
   end
 
   def test_words
-    assert_match /1\+|[ a-z]+/i, Faker::HipsterIpsum.words.join(" ")
+    assert_match TEST_REGEX, Faker::HipsterIpsum.words.join(" ")
   end
 
   def test_word
-    assert_match /1\+|[a-z]+/i, Faker::HipsterIpsum.word
+    assert_match TEST_REGEX, Faker::HipsterIpsum.word
   end
 end
