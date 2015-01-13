6d3843106d7a214777c8af4d3483d91671d9a907
Shim Won <marocchino@gmail.com>
2015-01-13 10:26:32 +0900

    NameCN has one letter first_name

--- test/test_name_cn.rb
+++ test/test_name_cn.rb
@@ -8,6 +8,14 @@ class TestFakerNameCN < Test::Unit::TestCase
   end
 
   def test_name
-    assert Faker::NameCN.name.length > 2
+    assert_match(/\A.{2,}\z/, @tester.name)
+  end
+
+  def test_first_name
+    assert_match(/\A.{1,2}\z/, @tester.first_name)
+  end
+
+  def test_last_name
+    assert_match(/\A.{1,5}\z/, @tester.last_name)
   end
 end
