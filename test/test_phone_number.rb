16bcfdfc597d4964da1f516cff841f2b73a9ba86
Will McDonald <wemcdonald@gmail.com>
2015-06-18 11:45:53 -0700

    Fix test typo

--- test/test_phone_number.rb
+++ test/test_phone_number.rb
@@ -12,11 +12,11 @@ class TestPhoneNumer < Test::Unit::TestCase
   end
 
   def test_imei
-    assert_match /^\d{15}$/, Faker::PhoneNumber.imei
+    assert_match /^\d{15}$/, FFaker::PhoneNumber.imei
   end
 
   def test_imei_with_sn
     sn = rand(1_000_000)
-    assert_match /^\d{8}#{sprintf('%06d', sn)}\d{1}$/, Faker::PhoneNumber.imei(sn)
+    assert_match /^\d{8}#{sprintf('%06d', sn)}\d{1}$/, FFaker::PhoneNumber.imei(sn)
   end
 end
