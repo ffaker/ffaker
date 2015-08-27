7592d5db2b6c22a3be5264f88840a1ae9003c878
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-08-27 01:00:47 -0300

    Add brazilian phones

--- lib/ffaker/phone_number_br.rb
+++ lib/ffaker/phone_number_br.rb
@@ -8,12 +8,12 @@ module FFaker
     extend ModuleUtils
     extend self
 
-    COUNTRY_PREFIX        = %w['+55' '0055' '0']
-    AREA_CODE             = %w['1#' '2#' '3#' '4#' '5#' '6#' '7#' '8#' '9#']
-    CARRIER_CODE          = %w['12' '14' '15' '21' '25' '31' '41' '43']
-    HOME_PHONE_PREFIX     = %w['2' '3' '4' '5']
-    MOBILE_PHONE_PREFIX   = %w['6' '7' '8' '9' '96' '97' '98' '99']
-    PHONE_NUMBER          = %w['#######' '###-####']
+    COUNTRY_PREFIX        = %w[+55 0055 0]
+    AREA_CODE             = %w[1# 2# 3# 4# 5# 6# 7# 8# 9#]
+    CARRIER_CODE          = %w[12 14 15 21 25 31 41 43]
+    HOME_PHONE_PREFIX     = %w[2 3 4 5]
+    MOBILE_PHONE_PREFIX   = %w[6 7 8 9 96 97 98 99]
+    PHONE_NUMBER          = %w[####### ###-####]
 
     def phone_number
       case rand(4)
@@ -58,7 +58,7 @@ module FFaker
         space,
         prefixes.sample,
         PHONE_NUMBER.sample
-      ].join('').squish
+      ].join('')
 
       FFaker.numerify p
     end
