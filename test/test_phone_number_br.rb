b0058cad49927773bc6fc5f6bd7cf854eb951493
Gustavo Souza <gustavo.do.r.souza@gmail.com>
2015-09-10 22:04:11 -0300

    improved tests of test_phone_number_br.rb

--- test/test_phone_number_br.rb
+++ test/test_phone_number_br.rb
@@ -9,30 +9,39 @@ class TestPhoneNumberBR < Test::Unit::TestCase
   end
 
   def test_phone_number
-    assert_match /^[1-9]\d\s?[9]?\d{4}[\-]?\d{4}$/, FFaker::PhoneNumberBR.phone_number
+    10.times do
+      assert_match /^[1-9]\d\s?[9]?\d{4}[\-]?\d{4}$/, @tester.phone_number
+    end
   end
 
   def test_home_work_phone_number
-    assert_match /^[1-9]\d\s?[2-5]\d{3}[\-]?\d{4}$/, FFaker::PhoneNumberBR.home_work_phone_number
+    assert_match /^[1-9]\d\s?[2-5]\d{3}[\-]?\d{4}$/,
+      @tester.home_work_phone_number
   end
 
   def test_mobile_phone_number
-    assert_match /^[1-9]\d\s?([6-9]|[9][6-9])\d{3}[\-]?\d{4}$/, FFaker::PhoneNumberBR.mobile_phone_number
+    assert_match /^[1-9]\d\s?([6-9]|[9][6-9])\d{3}[\-]?\d{4}$/,
+      @tester.mobile_phone_number
   end
 
   def test_international_phone_number
-    assert_match /^\+55\s?[1-9]\d\s?[9]?\d{4}[\-]?\d{4}$/, FFaker::PhoneNumberBR.international_phone_number
+    10.times do
+      assert_match /^\+55\s?[1-9]\d\s?[9]?\d{4}[\-]?\d{4}$/,
+        @tester.international_phone_number
+    end
   end
 
   def test_international_mobile_phone_number
-    assert_match /^\+55\s?[1-9]\d\s?([6-9]|[9][6-9])\d{3}[\-]?\d{4}$/, FFaker::PhoneNumberBR.international_mobile_phone_number
+    assert_match /^\+55\s?[1-9]\d\s?([6-9]|[9][6-9])\d{3}[\-]?\d{4}$/,
+      @tester.international_mobile_phone_number
   end
 
   def test_international_home_work_phone_number
-    assert_match /^\+55\s?[1-9]\d\s?[2-5]\d{3}[\-]?\d{4}$/, FFaker::PhoneNumberBR.international_home_work_phone_number
+    assert_match /^\+55\s?[1-9]\d\s?[2-5]\d{3}[\-]?\d{4}$/,
+      @tester.international_home_work_phone_number
   end
 
   def test_country_code
-    assert_match '+55', FFaker::PhoneNumberBR.country_code
+    assert_match '+55', @tester.country_code
   end
 end
