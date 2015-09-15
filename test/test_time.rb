d1469f4d62e6b480100131bde14f27b730c26196
Shim Won <marocchino@gmail.com>
2015-09-16 07:42:34 +0900

    Stricter regexps

--- test/test_time.rb
+++ test/test_time.rb
@@ -8,12 +8,12 @@ class TestFakerTime < Test::Unit::TestCase
   end
 
   def test_date
-    date_regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [-|+]\d{4}$/
+    date_regex = /\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [-|+]\d{4}\z/
     assert_match date_regex, @tester.date.to_s
   end
 
   def test_month
-    month_regex = /(January|February|March|April|May|June|July|August|September|October|November|December)/
+    month_regex = /\A(?:January|February|March|April|May|June|July|August|September|October|November|December)\z/
     assert_match month_regex, @tester.month
   end
 end
