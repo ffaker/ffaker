3fe0a1c45ff7cc404d0eb2ad553b66c787c3253d
Shim Won <marocchino@gmail.com>
2015-09-22 09:24:10 +0900

    Stricter test for address_da

--- test/test_address_da.rb
+++ test/test_address_da.rb
@@ -4,38 +4,38 @@ require 'helper'
 
 class TestAddressDA < Test::Unit::TestCase
   def test_zip_code
-    assert_match /^\d{4}$/, FFaker::AddressDA.zip_code
+    assert_match /\A\d{4}\z/, FFaker::AddressDA.zip_code
   end
 
   def test_post_nr
-    assert_match /^\d{4}$/, FFaker::AddressDA.post_nr
+    assert_match /\A\d{4}\z/, FFaker::AddressDA.post_nr
   end
 
   def test_state
-    assert_match /[ a-z]/, FFaker::AddressDA.state
+    assert_match /\A[-ÆØÅæøåa-z]+\z/i, FFaker::AddressDA.state
   end
 
   def test_kommune
-    assert_match /[ a-z]/, FFaker::AddressDA.kommune
+    assert_match /\A[-ÆØÅæøåa-z]+\z/i, FFaker::AddressDA.kommune
   end
 
-  def region
-    assert_match /[ a-z]/, FFaker::AddressDA.region
+  def test_region
+    assert_match /\A[ÆØÅæøåa-z]+\z/i, FFaker::AddressDA.region
   end
 
   def test_city
-    assert_match /[ a-z]+/, FFaker::AddressDA.city
+    assert_match /\A[- ÆØÅæøåla-z]+\z/i, FFaker::AddressDA.city
   end
 
   def test_street_name
-    assert_match /[ a-z]+/, FFaker::AddressDA.street_name
+    assert_match /\A[. éÆØÅæøåla-z0-9]+\z/i, FFaker::AddressDA.street_name
   end
 
   def test_street_address
-    assert_match /[ a-z]+/, FFaker::AddressDA.street_address
+    assert_match /\A[. éÆØÅæøåla-z0-9]+\z/i, FFaker::AddressDA.street_address
   end
 
   def test_full_address
-    assert_match /[ a-z]+/, FFaker::AddressDA.full_address
+    assert_match /\A[. éÆØÅæøåla-z0-9]+ DANMARK\z/i, FFaker::AddressDA.full_address
   end
 end
