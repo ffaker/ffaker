75edda101afdd840cc52b478cd9509354c260d4e
justynjozwiak <justyn.jozwiak@netguru.pl>
2015-11-01 11:10:00 +0100

    add test for hex colors

--- test/test_color.rb
+++ test/test_color.rb
@@ -6,4 +6,8 @@ class TestColor < Test::Unit::TestCase
   def test_name
     assert_match /\A[a-z]+\z/, FFaker::Color.name
   end
+
+  def test_hex
+    assert_match /(?<=#)(?<!^)\h{6}/, FFaker::Color.hex
+  end
 end
