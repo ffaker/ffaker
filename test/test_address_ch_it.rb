ed4bc57a8d2f86add1cf034c97b33b1d95fb3479
Hannes Maack <hmaack@rocknruby.it>
2016-01-12 12:54:58 +0100

    test all address

--- test/test_address_ch_it.rb
+++ test/test_address_ch_it.rb
@@ -4,6 +4,8 @@ require 'helper'
 
 class TestAddressCHIT < Test::Unit::TestCase
   def test_ch_it_canton
-    assert_match /\A[- a-zàèéìòóù]+\z/i, FFaker::AddressCHIT.canton
+    FFaker::AddressCHIT::CANTON.each do |canton|
+      assert_match /\A[- a-zàâ]+\z/i, canton
+    end
   end
 end
