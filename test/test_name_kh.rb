bfcdd2d89877c7890e4201521be2a5ad1082485b
Panh Phanith <panhphanith.kh@gmail.com>
2016-03-28 15:52:14 +0700

    use snake case in test

--- test/test_name_kh.rb
+++ test/test_name_kh.rb
@@ -2,24 +2,24 @@ require 'helper'
 
 class TestFakerNameKH < Test::Unit::TestCase
   def setup
-    @nameKH = FFaker::NameKH
+    @name_KH = FFaker::NameKH
   end
 
   def test_last_name
-    assert_include @nameKH::LAST_NAMES, @nameKH.last_name
+    assert_include @name_KH::LAST_NAMES, @name_KH.last_name
   end
 
   def test_first_name
-    assert_include @nameKH::FIRST_NAMES, @nameKH.first_name
+    assert_include @name_KH::FIRST_NAMES, @name_KH.first_name
   end
 
   def test_nick_name
-    assert_include @nameKH::NICK_NAMES, @nameKH.nick_name
+    assert_include @name_KH::NICK_NAMES, @name_KH.nick_name
   end
 
   def test_name
-    last_name, first_name = @nameKH.name.split(' ')
-    assert_include @nameKH::FIRST_NAMES, first_name
-    assert_include @nameKH::LAST_NAMES, last_name
+    last_name, first_name = @name_KH.name.split(' ')
+    assert_include @name_KH::FIRST_NAMES, first_name
+    assert_include @name_KH::LAST_NAMES, last_name
   end
 end
