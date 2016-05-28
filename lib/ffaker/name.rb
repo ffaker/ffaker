65fc3b7cc407752b76e47a88f482d56970efc064
Navneet Pandey <npandey057@gmail.com>
2016-05-28 14:45:10 +0530

    Add male and female name for english

--- lib/ffaker/name.rb
+++ lib/ffaker/name.rb
@@ -5,19 +5,72 @@ module FFaker
     extend ModuleUtils
     extend self
 
-    PREFIXES = %w(Mr. Mrs. Ms. Miss Dr.).freeze
+    MALE_PREFIXES = %w(Mr.).freeze
+    FEMALE_PREFIXES = %w(Mrs. Miss.).freeze
+    OTHER_PREFIXES =  %w(Ms. Dr.).freeze
+    PREFIXES = (MALE_PREFIXES + FEMALE_PREFIXES + OTHER_PREFIXES).freeze
+    FIRST_NAMES = (FIRST_NAMES_FEMALE + FIRST_NAMES_MALE).freeze
     SUFFIXES = %w(Jr. Sr. I II III IV V MD DDS PhD DVM).freeze
 
     def name
-      case rand(10)
-      when 0 then "#{prefix} #{first_name} #{last_name}"
-      when 1 then "#{first_name} #{last_name} #{suffix}"
-      else        "#{first_name} #{last_name}"
-      end
+      "#{first_name} #{last_name}"
+    end
+
+    def name_with_prefix
+      [
+        female_name_with_prefix,
+        male_name_with_prefix
+      ].sample
+    end
+
+    def name_with_suffix
+      [
+        female_name_with_suffix,
+        male_name_with_suffix
+      ].sample
+    end
+
+    def name_with_prefix_suffix
+      [
+        female_name_with_prefix_suffix,
+        male_name_with_prefix_suffix
+      ].sample
+    end
+
+    def female_name_with_prefix
+      "#{female_prefix} #{first_name_female} #{last_name}"
+    end
+
+    def male_name_with_prefix
+      "#{male_prefix} #{first_name_male} #{last_name}"
+    end
+
+    def female_name_with_suffix
+      "#{first_name_female} #{last_name} #{suffix}"
+    end
+
+    def male_name_with_suffix
+      "#{first_name_male} #{last_name} #{suffix}"
+    end
+
+    def female_name_with_prefix_suffix
+      "#{female_prefix} #{first_name_female} #{last_name} #{suffix}"
+    end
+
+    def male_name_with_prefix_suffix
+      "#{male_prefix} #{first_name_male} #{last_name} #{suffix}"
     end
 
     def first_name
       FIRST_NAMES.sample
+    end
+
+    def first_name_female
+      FIRST_NAMES_FEMALE.sample
+    end
+
+    def first_name_male
+      FIRST_NAMES_MALE.sample
     end
 
     def last_name
@@ -26,6 +79,18 @@ module FFaker
 
     def prefix
       PREFIXES.sample
+    end
+
+    def female_prefix
+      FEMALE_PREFIXES.sample
+    end
+
+    def male_prefix
+      MALE_PREFIXES.sample
+    end
+
+    def other_prefix
+      OTHER_PREFIXES.sample
     end
 
     def suffix
--- test/test_name.rb
+++ test/test_name.rb
@@ -11,8 +11,102 @@ class TestFakerName < Test::Unit::TestCase
     assert_match(/(\w+\.? ?){2,3}/, @tester.name)
   end
 
+  def test_name_with_prefix
+    prefix, name, last_name = @tester.name_with_prefix.split(/\s+/)
+    assert_include(@tester::PREFIXES, prefix)
+    assert(!name.empty?)
+    refute_empty(last_name)
+  end
+
+  def test_female_name_with_prefix
+    prefix, name, last_name = @tester.female_name_with_prefix.split(/\s+/)
+    assert_include(@tester::FEMALE_PREFIXES, prefix)
+    assert_include(@tester::FIRST_NAMES_FEMALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_male_name_with_prefix
+    prefix, name, last_name = @tester.male_name_with_prefix.split(/\s+/)
+    assert_include(@tester::MALE_PREFIXES, prefix)
+    assert_include(@tester::FIRST_NAMES_MALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_name_with_suffix
+    name, last_name, suffix = @tester.name_with_suffix.split(/\s+/)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert(!name.empty?)
+    refute_empty(last_name)
+  end
+
+  def test_female_name_with_suffix
+    name, last_name, suffix = @tester.female_name_with_suffix.split(/\s+/)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert_include(@tester::FIRST_NAMES_FEMALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_male_name_with_suffix
+    name, last_name, suffix = @tester.male_name_with_suffix.split(/\s+/)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert_include(@tester::FIRST_NAMES_MALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_name_with_prefix_suffix
+    prefix, name, last_name, suffix = @tester.name_with_prefix_suffix.split(/\s+/)
+    assert_include(@tester::PREFIXES, prefix)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert(!name.empty?)
+    refute_empty(last_name)
+  end
+
+  def test_female_name_with_prefix_suffix
+    prefix, name, last_name, suffix = @tester.female_name_with_prefix_suffix.split(/\s+/)
+    assert_include(@tester::FEMALE_PREFIXES, prefix)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert_include(@tester::FIRST_NAMES_FEMALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_male_name_with_prefix_suffix
+    prefix, name, last_name, suffix = @tester.male_name_with_prefix_suffix.split(/\s+/)
+    assert_include(@tester::MALE_PREFIXES, prefix)
+    assert_include(@tester::SUFFIXES, suffix)
+    assert_include(@tester::FIRST_NAMES_MALE, name)
+    refute_empty(last_name)
+  end
+
+  def test_first_name
+    assert_include(@tester::FIRST_NAMES, @tester.first_name)
+  end
+
+  def test_first_name_female
+    assert_include @tester::FIRST_NAMES_FEMALE, @tester.first_name_female
+  end
+
+  def test_first_name_male
+    assert_include @tester::FIRST_NAMES_MALE, @tester.first_name_male
+  end
+
+  def test_last_name
+    assert_include(@tester::LAST_NAMES, @tester.last_name)
+  end
+
   def test_prefix
-    assert_match(/[A-Z][a-z]+\.?/, @tester.prefix)
+    assert_include(@tester::PREFIXES, @tester.prefix)
+  end
+
+  def test_female_prefix
+    assert_include(@tester::FEMALE_PREFIXES, @tester.female_prefix)
+  end
+
+  def test_male_prefix
+    assert_include(@tester::MALE_PREFIXES, @tester.male_prefix)
+  end
+
+  def test_other_prefix
+    assert_include(@tester::OTHER_PREFIXES, @tester.other_prefix)
   end
 
   def test_suffix
