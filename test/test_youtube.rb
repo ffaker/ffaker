a5d52a20a79fa32beb531e4f11caa6eebf506b8a
Andy Klimczak <andyklimczak@fastmail.com>
2016-06-04 16:43:04 -0400

    FFaker Youtube

---
+++ test/test_youtube.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'helper'
+
+class YoutubeTest < Test::Unit::TestCase
+  def test_url
+    assert_match(%r(www\.youtube\.com/watch\?v=\S{11}), FFaker::Youtube.url)
+  end
+
+  def test_share_url
+    assert_match(%r(youtu\.be/\S{11}), FFaker::Youtube.share_url)
+  end
+
+  def test_embed_url
+    assert_match(%r(www\.youtube\.com/embed/\S{11}), FFaker::Youtube.embed_url)
+  end
+
+  def test_video_id
+    assert_match(/\S{11}/, FFaker::Youtube.video_id)
+  end
+end
