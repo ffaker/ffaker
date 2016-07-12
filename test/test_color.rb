a0682613f6336ea14cce31f92ac350bc2b1fcd18
D.P. Hurley <pair+hurley.dannyp@gmail.com@pivotal.io>
2016-07-12 18:55:29 -0400

    Update regex assertions to make Hound happy

--- test/test_color.rb
+++ test/test_color.rb
@@ -17,23 +17,23 @@ class TestColor < Test::Unit::TestCase
     assert fake_array.size == 4
 
     red_value = fake_array[0]
-    assert_match /^([0-9]{1,3})$/, red_value.to_s
+    assert_match(/^([0-9]{1,3})$/, red_value.to_s)
     assert red_value.between? 0, 255
 
     green_value = fake_array[1]
-    assert_match /^([0-9]{1,3})$/, green_value.to_s
+    assert_match(/^([0-9]{1,3})$/, green_value.to_s)
     assert green_value.between? 0, 255
 
     blue_value = fake_array[2]
-    assert_match /^([0-9]{1,3})$/, blue_value.to_s
+    assert_match(/^([0-9]{1,3})$/, blue_value.to_s)
     assert blue_value.between? 0, 255
 
     opacity_value = fake_array[3]
-    assert_match /^([0-9].[0-9]{2})$/, opacity_value.to_s
+    assert_match(/^([0-9].[0-9]{2})$/, opacity_value.to_s)
     assert opacity_value.between? 0.0, 1.0
   end
 
   def test_rgba_list
-    assert_match /^([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}),([0-9].[0-9]{2})$/, FFaker::Color.rgba_list
+    assert_match(/^([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}),([0-9].[0-9]{2})$/, FFaker::Color.rgba_list)
   end
 end
