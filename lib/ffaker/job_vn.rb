64605a4a8990c8f609e56387654d5c59d946e719
Vinh Nguyen <vinhnguyen@Vinh-Nguyen.local>
2016-10-13 17:03:21 +0800

    Add jobs in Vietnam

---
+++ lib/ffaker/job_vn.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+module FFaker
+  module JobVN
+    extend ModuleUtils
+    extend self
+
+    def title
+      fetch_sample(JOB_NOUNS)
+    end
+  end
+end
---
+++ test/test_job_vn.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+
+require 'helper'
+
+class TestFakerJobVN < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(FFaker::JobVN, :title)
+
+  def setup
+    @tester = FFaker::JobVN
+  end
+
+  def test_title
+    assert @tester.title.length >= 1
+  end
+
+  def test_nouns
+    puts @tester.title
+    assert_kind_of Array, @tester::JOB_NOUNS
+  end
+end
