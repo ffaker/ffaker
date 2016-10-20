73b3e36978d80ff046800a0c60ab4fad1c4063b6
Josh Dvir <shuky.dvir@gmail.com>
2016-10-20 17:12:46 +0100

    adding aws ec2 fake data

---
+++ lib/ffaker/aws.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+module FFaker
+  module AWS
+    extend ModuleUtils
+    extend self
+
+    def instance_type
+      fetch_sample(EC2_INSTANCE_TYPE)
+    end
+
+    def offering_type
+      fetch_sample(EC2_RESERVED_OFFERING_TYPE)
+    end
+
+    def instance_tenancy
+      fetch_sample(EC2_RESERVED_INSTANCE_TENANCY)
+    end
+
+    def product_description
+      fetch_sample(EC2_RESERVED_PRODUCT_DESCRIPTION)
+    end
+  end
+end
---
+++ test/test_aws.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+require 'helper'
+
+class TestAWS < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(FFaker::AWS, :instance_type)
+
+  def test_instance_type
+    assert_match(/^[a-z]{1}[0-9]{1}\.[a-z]+/, FFaker::AWS.instance_type)
+  end
+
+  def test_offering_type
+    assert_match(/\A[ a-zA-Z]+\z/i, FFaker::AWS.offering_type)
+  end
+
+  def test_instance_tenancy
+    assert_match(/\A[default|dedicated|host]+\z/i, FFaker::AWS.instance_tenancy)
+  end
+
+  def test_product_description
+    assert_match(/\A[ a-zA-Z\(\)]+\z/i, FFaker::AWS.product_description)
+  end
+end
