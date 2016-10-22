e6ab6e77ff267f1dc7e22f99653a10fa10a17e88
Josh Dvir <joshdvir@users.noreply.github.com>
2016-10-22 11:12:42 +0100

    adding aws ec2 fake data (#303)
    * adding aws ec2 fake data
    
    * fixing broken tests
    
    * missing /
    
    * round brackets
    
    * better tests syntax

---
+++ test/test_aws.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+require 'helper'
+
+class TestAWS < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(FFaker::AWS, :instance_type)
+
+  def test_instance_type
+    assert_match(%r{\A[a-z]+\d\.\d*[a-z]+}, FFaker::AWS.instance_type)
+  end
+
+  def test_offering_type
+    assert_match(%r{\A[ a-zA-Z]+\z}i, FFaker::AWS.offering_type)
+  end
+
+  def test_instance_tenancy
+    assert_match(%r{\A(?:default|dedicated|host)+\z}i, FFaker::AWS.instance_tenancy)
+  end
+
+  def test_product_description
+    assert_match(%r{\A[ \/a-zA-Z\(\)]+\z}i, FFaker::AWS.product_description)
+  end
+end
