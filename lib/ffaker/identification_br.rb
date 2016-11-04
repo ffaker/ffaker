eac8a5d5bddef9ae9625a705676cdf101239130c
Rafael G <gibim6@gmail.com>
2016-11-04 17:41:28 -0200

    Adds valid brazilian CPF/CNPJ generators (#306)
    * Added valid cpf and cnpj generators
    
    * Rubocop fixes for identification_br

--- lib/ffaker/identification_br.rb
+++ lib/ffaker/identification_br.rb
@@ -6,13 +6,38 @@ module FFaker
     extend self
 
     GENDERS = %w(Feminino Masculino).freeze
+    CPF_VERIFICATION_MULTIPLIERS = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2].freeze
+    CNPJ_VERIFICATION_MULTIPLIERS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2].freeze
 
     def cpf
-      FFaker.numerify('###########')
+      cpf = FFaker.numerify('#########')
+      validators = cpf_verification_digits(cpf)
+      cpf + validators.join
     end
 
     def pretty_cpf
-      FFaker.numerify('###.###.###-##')
+      cpf_numbers = cpf
+      format('%s.%s.%s-%s',
+             cpf_numbers[0..2],
+             cpf_numbers[3..5],
+             cpf_numbers[6..8],
+             cpf_numbers[9..10])
+    end
+
+    def cnpj
+      cnpj = FFaker.numerify('############')
+      validators = cnpj_verification_digits(cnpj)
+      cnpj + validators.join
+    end
+
+    def pretty_cnpj
+      cnpj_numbers = cnpj.to_s.rjust(14, '0')
+      format('%s.%s.%s/%s-%s',
+             cnpj_numbers[0..1],
+             cnpj_numbers[2..4],
+             cnpj_numbers[5..7],
+             cnpj_numbers[8..11],
+             cnpj_numbers[12..14])
     end
 
     def rg
@@ -25,6 +50,35 @@ module FFaker
 
     def gender
       fetch_sample(GENDERS)
+    end
+
+    def cpf_verification_digits(cpf)
+      document_verification_digits(CPF_VERIFICATION_MULTIPLIERS, cpf)
+    end
+
+    def cnpj_verification_digits(cnpj)
+      document_verification_digits(CNPJ_VERIFICATION_MULTIPLIERS, cnpj)
+    end
+
+    private
+
+    def document_verification_digits(verification_multipliers, document = nil)
+      return false if document.nil?
+
+      document_digits = document.to_s.scan(/[0-9]/).map(&:to_i)
+      validation_digits = []
+
+      if document_digits.size + 1 == verification_multipliers.size && document_digits.uniq.size > 1
+        0.upto(1) do |i|
+          sum = verification_multipliers.drop(1 - i).each_with_index.inject(0) do |val, (value, j)|
+            val + document_digits[j] * value
+          end
+          validation_digits[i] = (sum %= 11) < 2 ? 0 : 11 - sum
+          document_digits.push(validation_digits[i])
+        end
+        return validation_digits
+      end
+      []
     end
   end
 end
--- test/test_identification_br.rb
+++ test/test_identification_br.rb
@@ -7,7 +7,7 @@ class TestFakerIdentificationBR < Test::Unit::TestCase
 
   assert_methods_are_deterministic(
     FFaker::IdentificationBR,
-    :cpf, :pretty_cpf, :rg, :pretty_rg, :gender
+    :cpf, :pretty_cpf, :cnpj, :pretty_cnpj, :rg, :pretty_rg, :gender
   )
 
   def setup
@@ -15,11 +15,25 @@ class TestFakerIdentificationBR < Test::Unit::TestCase
   end
 
   def test_cpf
-    assert_match(/\A\d{11}\z/, @tester.cpf)
+    cpf = @tester.cpf
+    verification_digits = @tester.cpf_verification_digits(cpf[0..8])
+    assert(cpf[9..10] == verification_digits.join)
+    assert_match(/\A\d{11}\z/, cpf)
   end
 
   def test_pretty_cpf
-    assert_match(/\A\d{3}.\d{3}.\d{3}-\d{2}\z/, @tester.pretty_cpf)
+    assert_match(/\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/, @tester.pretty_cpf)
+  end
+
+  def test_cnpj
+    cnpj = @tester.cnpj
+    verification_digits = @tester.cnpj_verification_digits(cnpj[0..11])
+    assert(cnpj[12..13] == verification_digits.join)
+    assert_match(/\A\d{14}\z/, cnpj)
+  end
+
+  def test_pretty_cnpj
+    assert_match(%r{\A\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}\z}, @tester.pretty_cnpj)
   end
 
   def test_rg
