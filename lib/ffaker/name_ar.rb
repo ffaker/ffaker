a9224ba43341920341bff0c5d9805436650531af
Moafak <moafak.m@gmail.com>
2017-01-01 12:35:42 +0200

    Added Arabic Names (#312)
    * Added Arabic Names
    
    * resolving comments after prev commit
    
    * resolving again
    
    * Add final new line to test_name_ar.rb, name_ar.rb

---
+++ lib/ffaker/name_ar.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+module FFaker
+  module NameAR
+    extend ModuleUtils
+    extend self
+
+    def first_name_male
+      fetch_sample(FIRST_NAMES_MALE)
+    end
+    
+    def first_name_female
+      fetch_sample(FIRST_NAMES_FEMALE)
+    end
+    
+    def last_name
+      fetch_sample(LAST_NAMES)
+    end
+    
+    def name_male
+      "#{first_name_male} #{last_name}"
+    end
+    
+    def name_female
+      "#{first_name_female} #{last_name}"
+    end
+  end
+end
---
+++ test/test_name_ar.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+require 'helper'
+
+class TestFakerNameAR < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::NameAR,
+    :first_name_male, :first_name_female, :last_name,
+    :name_male, :name_female
+  )
+
+  def setup
+    @tester = FFaker::NameAR
+  end
+
+  def test_first_name_male
+    assert_include(@tester::FIRST_NAMES_MALE, @tester.first_name_male)
+  end
+  
+  def test_first_name_female
+    assert_include(@tester::FIRST_NAMES_FEMALE, @tester.first_name_female)
+  end
+  
+  def test_last_name
+    assert_include(@tester::LAST_NAMES, @tester.last_name)
+  end
+  
+  def test_name_male
+    first_name, last_name = @tester.name_male.split(/\s+/, 2)
+    assert_include(@tester::FIRST_NAMES_MALE, first_name)
+    assert_include(@tester::LAST_NAMES, last_name)
+  end
+  
+  def test_name_female
+    first_name, last_name = @tester.name_female.split(/\s+/, 2)
+    assert_include(@tester::FIRST_NAMES_FEMALE, first_name)
+    assert_include(@tester::LAST_NAMES, last_name)
+  end
+end
