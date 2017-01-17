1731dfab67a61eb3c87a25c9f4d710de626a5e25
Kojin <kojin.oshiba@gmail.com>
2017-01-17 01:33:47 -0800

    Add japanese address (#314)
    * Add address_ja.rb and its test
    
    * add AddressJA to reference
    
    * fix code style as suggested
    
    * fix code style as suggested
    
    * remove private from addressja test helper
    
    * fix test_prefecture regexp

---
+++ lib/ffaker/address_ja.rb
@@ -0,0 +1,86 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'ffaker/address'
+
+module FFaker
+  module AddressJA
+    include FFaker::Address
+
+    extend ModuleUtils
+    extend self
+
+    ZIP_FORMATS = ['###-####'].freeze
+    LAND_NUMBER_FORMATS = ['#丁目#番#号', '###'].freeze
+
+    def address
+      fetch_sample([
+        "#{postal_code} #{prefecture}#{city}#{street}#{land_number}",
+        "#{postal_code} #{prefecture}#{county}#{street}#{land_number}",
+        "#{postal_code} #{prefecture}#{designated_city}#{ward}#{street}#{land_number}",
+        "#{postal_code} 東京都#{tokyo_ward}#{street}#{land_number}"
+      ])
+    end
+
+    def other_address
+      fetch_sample([
+        "#{postal_code} #{prefecture}#{city}#{street}#{land_number}",
+        "#{postal_code} #{prefecture}#{county}#{street}#{land_number}"
+      ])
+    end
+
+    def designated_city_address
+      fetch_sample([
+                      # 愛知県名古屋市中区錦3丁目24番4号
+                      "#{postal_code} #{prefecture}#{designated_city}#{ward}#{street}#{land_number}"
+                   ])
+    end
+
+    def tokyo_ward_address
+      fetch_sample([
+                      # 東京都千代田区永田町2丁目2番1号
+                      "#{postal_code} 東京都#{tokyo_ward}#{street}#{land_number}"
+                   ])
+    end
+
+    def postal_code
+      FFaker.numerify(fetch_sample(ZIP_FORMATS))
+    end
+
+    def land_number
+      FFaker.numerify(fetch_sample(LAND_NUMBER_FORMATS))
+    end
+
+    def street
+      FFaker.numerify(fetch_sample(CITY))
+    end
+
+    def tokyo_ward
+      fetch_sample(TOKYO_WARD)
+    end
+
+    def ward
+      fetch_sample(WARD)
+    end
+
+    def village
+      fetch_sample(VILLAGE)
+    end
+
+    def designated_city
+      fetch_sample(CITY)
+    end
+
+    def city
+      fetch_sample(CITY)
+    end
+
+    def county
+      fetch_sample(COUNTY)
+    end
+
+    def prefecture
+      fetch_sample(PREFECTURE)
+    end
+  end
+end
---
+++ test/test_address_ja.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+# frozen_string_literal: true
+require 'helper'
+
+class TestAddressJA < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::AddressJA,
+    :address,  :other_address, :designated_city_address, :tokyo_ward_address,
+    :zip_code, :land_number, :tokyo_ward, :ward, :village, :city, :county,
+    :prefecture
+  )
+
+  def test_postal_code
+    assert_match(/^\d{3}\-\d{4}$/, FFaker::AddressJA.postal_code)
+  end
+
+  def test_land_number
+    assert_match(/^\d丁目\d番\d号|\d{3}$/, FFaker::AddressJA.land_number)
+  end
+
+  def test_street
+    assert_match(japanese_regex(''), FFaker::AddressJA.street)
+  end
+
+  def test_tokyo_ward
+    assert_match(japanese_regex('区'), FFaker::AddressJA.tokyo_ward)
+  end
+
+  def test_ward
+    assert_match(japanese_regex('区'), FFaker::AddressJA.ward)
+  end
+
+  def test_village
+    assert_match(japanese_regex('村'), FFaker::AddressJA.village)
+  end
+
+  def test_designated_city
+    assert_match(japanese_regex('市'), FFaker::AddressJA.city)
+  end
+
+  def test_city
+    assert_match(japanese_regex('市'), FFaker::AddressJA.city)
+  end
+
+  def test_county
+    assert_match(japanese_regex('郡'), FFaker::AddressJA.county)
+  end
+
+  def test_prefecture
+    assert_match(japanese_regex('[都道府県]'), FFaker::AddressJA.prefecture)
+  end
+
+  def japanese_regex(word)
+    /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+#{word}\z/
+  end
+end
