f1effb7688232e2984470ce75d549d9f2b6693ee
Andrea Schiavini <metalelf0@Valhalla.homenet.telecomitalia.it>
2017-02-15 11:34:29 +0100

    Provides correct results, but not deterministic for serial code

--- lib/ffaker/phone_number.rb
+++ lib/ffaker/phone_number.rb
@@ -56,11 +56,17 @@ module FFaker
       # AA-BBBBBB-CCCCCC-D
       characters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       rbi_codes  = %w(01 10 30 33 35 44 45 49 50 51 52 53 54 86 91 98 99)
+      serial_number ||= rand(1_000_000)
+      serial_number = sprintf("%06d", serial_number).chars.map(&:to_i)
       first_two_chars = rbi_codes.sample
       characters[0] = first_two_chars.chars.map(&:to_i)[0]
       characters[1] = first_two_chars.chars.map(&:to_i)[1]
-      2.upto(13) do |current_position|
+      # serial number part
+      2.upto(7) do |current_position|
         characters[current_position] = (0..9).to_a.sample
+      end
+      8.upto(13) do |current_position|
+        characters[current_position] = serial_number[current_position - 8]
       end
       current_checksum = characters.reverse.each_with_index.inject(0) do |sum, (digit, i)|
         digit *= 2 if i.odd?
