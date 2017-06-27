b251a6e8dc93a1696360d3d8f00539b88ca35f0f
Nick Bell <bell.nicholas.p@gmail.com>
2017-06-26 21:51:54 -0400

    Add Animals (#326)
    * added basic animal list
    
    * fixed error
    
    * added animals

---
+++ lib/ffaker/animals.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+module FFaker
+  module Animal
+    extend ModuleUtils
+    extend self
+
+    def common_name
+      fetch_sample(COMMON_NAMES)
+    end
+    
+  end
+end
---
+++ test/test_animal.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+require 'helper'
+
+class TestAnimals < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(FFaker::Animal, :common_name)
+
+  def setup
+    @tester = FFaker::Animal
+  end
+
+  def test_name
+    assert_include @tester::COMMON_NAMES, @tester.common_name
+  end
+end
+
