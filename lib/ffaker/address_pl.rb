d8b406b1624b447f0c13e37580db2a2d61132eca
Walerian <walerian.sobczak@gmail.com>
2017-06-27 03:50:15 +0200

    Added Polish addresses (#319)
    * Added Polish addresses
    
    * Improved rand style

---
+++ lib/ffaker/address_pl.rb
@@ -0,0 +1,85 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module FFaker
+  # Author Walerian Sobczak <walerian.sobczak@gmail.com> github.com/walerian777
+  # Inspirations:
+  # TERYT registry, available at: http://www.stat.gov.pl/broker/access/index.jspa
+  module AddressPL
+    extend ModuleUtils
+    extend self
+
+    STREET_PREFIXES = %w(al. ul.).freeze
+    SQUARE_PREFIXES = %w(bulwar pasaż plac skwer zaułek).freeze
+
+    # Full Polish address with country name in French (according to Universal Postal Union).
+    def full_address(include_country = false, include_secondary = false)
+      address = "#{street_address(include_secondary)} #{postal_code} #{city}"
+      address = "#{address} POLOGNE" if include_country
+      address
+    end
+
+    def street_address(include_secondary = false)
+      address = "#{street_name} #{building_number}"
+      address = "#{address}#{secondary_number}" if include_secondary
+      address
+    end
+
+    def street_name
+      fetch_sample([street, square])
+    end
+
+    def street
+      "#{street_prefix} #{fetch_sample(STREET)}"
+    end
+
+    def square
+      "#{square_prefix} #{fetch_sample(SQUARE)}"
+    end
+
+    # A voivodeship is an administrative area in Poland - a Polish equivalent of state.
+    def voivodeship
+      fetch_sample(VOIVODESHIP)
+    end
+
+    alias state voivodeship
+    alias province voivodeship
+
+    def voivodeship_abbr
+      fetch_sample(VOIVODESHIP_ABBR)
+    end
+
+    def voivodeship_capital_city
+      fetch_sample(VOIVODESHIP_CAPITAL_CITY)
+    end
+
+    def city
+      fetch_sample(CITY)
+    end
+
+    def building_number # :nodoc:
+      rand(1..199)
+    end
+
+    def secondary_number # :nodoc:
+      case rand(2)
+      when 0 then " m. #{building_number}"
+      when 1 then "/#{building_number}"
+      end
+    end
+
+    def postal_code
+      FFaker.numerify('##-###')
+    end
+
+    alias zip_code postal_code
+
+    def street_prefix
+      fetch_sample(STREET_PREFIXES)
+    end
+
+    def square_prefix
+      fetch_sample(SQUARE_PREFIXES)
+    end
+  end
+end
---
+++ test/test_address_pl.rb
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+require 'helper'
+
+class TestFakerAddressPL < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::AddressPL,
+    :full_address, :street_address, :street_name, :street, :square, :voivodeship,
+    :voivodeship_abbr, :voivodeship_capital_city, :city, :building_number,
+    :secondary_number, :postal_code, :street_prefix, :square_prefix
+  )
+
+  def setup
+    @tester = FFaker::AddressPL
+  end
+
+  def test_full_address
+    assert_match(/[a-z]/, @tester.full_address)
+  end
+
+  def test_full_address_includes_country
+    assert_match(/POLOGNE/, @tester.full_address(true))
+  end
+
+  def street_address
+    assert_match(/[a-z]/, @tester.street_address)
+  end
+
+  def street_name
+    prefix, street = @tester.street_name.split
+    assert_include(@tester::STREET_PREFIXES + @tester::SQUARE_PREFIXES, prefix)
+    assert_include(@tester::STREET + @tester::SQUARE, street)
+  end
+
+  def street
+    assert_include(@tester::STREET, @tester.street)
+  end
+
+  def square
+    assert_include(@tester::SQUARE, @tester.square)
+  end
+
+  def voivodeship
+    assert_include(@tester::VOIVODESHIP, @tester.voivodeship)
+  end
+
+  def voivodeship_abbr
+    assert_include(@tester::VOIVODESHIP_ABBR, @tester.voivodeship_abbr)
+  end
+
+  def voivodeship_capital_city
+    assert_include(@tester::VOIVODESHIP_CAPITAL_CITY, @tester.voivodeship_capital_city)
+  end
+
+  def city
+    assert_include(@tester::CITY, @tester.city)
+  end
+
+  def building
+    assert_match(/\d{1,3}/, @tester.building)
+  end
+
+  def secondary_number
+    assert_match(/\d{1,3}/, @tester.secondary_number.split.last)
+  end
+
+  def postal_code
+    assert_match(/\d{2}-\d{3}/, @tester.postal_code)
+  end
+
+  def street_prefix
+    assert_include(@tester::STREET_PREFIXES, @tester.square_prefix)
+  end
+
+  def square_prefix
+    assert_include(@tester::SQUARE_PREFIXES, @tester.square_prefix)
+  end
+end
