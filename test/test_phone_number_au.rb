66174d10575b21dc41d25aac0ef84c26a944fd24
Shim Won <marocchino@gmail.com>
2017-06-27 14:13:55 +0900

    Lint test

--- test/test_phone_number_au.rb
+++ test/test_phone_number_au.rb
@@ -20,44 +20,47 @@ class TestPhoneNumberAU < Test::Unit::TestCase
   end
 
   def test_mobile_phone_prefix
-    assert FFaker::PhoneNumberAU::MobileOperatorsPrefix.include?(@tester.mobile_phone_prefix)
+    assert_include(FFaker::PhoneNumberAU::MobileOperatorsPrefix,
+                   @tester.mobile_phone_prefix)
   end
 
   def test_home_work_phone_prefix
-    assert FFaker::PhoneNumberAU::HomeWorkOperatorsPrefix.include?(@tester.home_work_phone_prefix)
+    assert_include(FFaker::PhoneNumberAU::HomeWorkOperatorsPrefix,
+                   @tester.home_work_phone_prefix)
   end
 
   def test_phone_prefix
-    assert FFaker::PhoneNumberAU::OperatorsPrefix.include?(@tester.phone_prefix)
+    assert_include(FFaker::PhoneNumberAU::OperatorsPrefix,
+                   @tester.phone_prefix)
   end
 
   def test_mobile_phone_number
-    3.times do 
-      assert_match(/(04|05)\d{2} \d{3} \d{3}/, FFaker::PhoneNumberAU.mobile_phone_number)
-    end
+    assert_match(/0[45]\d{2} \d{3} \d{3}/,
+                 FFaker::PhoneNumberAU.mobile_phone_number)
   end
 
   def test_home_work_phone_number
-    assert_match(/\(\d{2}\) \d{4} \d{4}/, FFaker::PhoneNumberAU.home_work_phone_number)
+    assert_match(/\(\d{2}\) \d{4} \d{4}/,
+                 FFaker::PhoneNumberAU.home_work_phone_number)
   end
 
   def test_phone_number
-    10.times do
-      assert_match(/((04|05)\d{1,2} \d{3} \d{3}|\(\d{2}\) \d{4} \d{4})/, FFaker::PhoneNumberAU.phone_number)
-    end
+    assert_match(/(?:0[45]\d{1,2} \d{3} \d{3}|\(\d{2}\) \d{4} \d{4})/,
+                 FFaker::PhoneNumberAU.phone_number)
   end
 
   def test_international_mobile_phone_number
-    assert_match(/\+61 \d{1,2} \d{4} \d{4}/, FFaker::PhoneNumberAU.international_mobile_phone_number)
+    assert_match(/\+61 \d{1,2} \d{4} \d{4}/,
+                 FFaker::PhoneNumberAU.international_mobile_phone_number)
   end
 
   def test_international_home_work_phone_number
-    assert_match(/\+61 \d{1,2} \d{4} \d{4}/, FFaker::PhoneNumberAU.international_home_work_phone_number)
+    assert_match(/\+61 \d{1,2} \d{4} \d{4}/,
+                 FFaker::PhoneNumberAU.international_home_work_phone_number)
   end
 
   def test_international_phone_number
-    10.times do
-      assert_match(/\+61 \d{1,2} \d{4} \d{4}/, FFaker::PhoneNumberAU.international_phone_number)
-    end
+    assert_match(/\+61 \d{1,2} \d{4} \d{4}/,
+                 FFaker::PhoneNumberAU.international_phone_number)
   end
 end
