41fee15c7ead953fe3627cbbaadafbe48e761ba5
Aaron <aaron.ciaghi@gmail.com>
2017-09-19 04:27:30 +0200

    Added CompanyIT#partita_iva method to generate valid Italian VAT numbers (#330)
    * added partita_iva method to generate valid Italian VAT numbers
    
    * fixed control digit calculation
    
    * fixed formatting to pass hound checks
    
    * hound doesn't like multiline expressions apparently

--- lib/ffaker/company_it.rb
+++ lib/ffaker/company_it.rb
@@ -23,5 +23,21 @@ module FFaker
     def prefix
       fetch_sample(PREFIXES)
     end
+
+    # Calculated using the algorithm at https://it.wikipedia.org/wiki/Partita_IVA
+    # to return a valid Partita IVA (Italian VAT number)
+    # @return a valid Italian VAT number (Partita IVA)
+    def partita_iva
+      matricola = Array.new(7) { rand(0..9) }
+      office_code = fetch_sample(PIVA_OFFICE_CODES)
+      base = matricola + office_code.chars.map(&:to_i)
+
+      # Thanks https://stackoverflow.com/a/9189731/1627766
+      sum = base.reverse.each_slice(2).flat_map { |x, y| [(x * 2).divmod(10), y || 0] }.flatten.reduce(:+)
+
+      control_digit = (10 - (sum % 10)) % 10
+
+      (base << control_digit).join
+    end
   end
 end
