ffaker/ffaker

commit 3b279e031af01238bf30a46489a6fc418d90f21a

auto lint

ffaker.gemspec
@@ -21,13 +21,13 @@ Gem::Specification.new do |s|
   s.email    = 'EmmanuelOga@gmail.com'
   s.homepage = 'http://github.com/ffaker/ffaker'
 
-  s.require_paths = %w(lib)
+  s.require_paths = %w[lib]
 
   s.rdoc_options = ['--charset=UTF-8']
-  s.extra_rdoc_files = %w(README.md LICENSE Changelog.md)
+  s.extra_rdoc_files = %w[README.md LICENSE Changelog.md]
 
   # = MANIFEST =
-  s.files = %w(
+  s.files = %w[
     Changelog.md
     Gemfile
     LICENSE
@@ -35,7 +35,7 @@ Gem::Specification.new do |s|
     REFERENCE.md
     Rakefile
     ffaker.gemspec
-  ) + Dir['lib/**/*', 'scripts/*']
+  ] + Dir['lib/**/*', 'scripts/*']
   # = MANIFEST =
 
   s.test_files = Dir['test/**/*']

commit 0b69bb397397df00ab4b0b629dcbc27d31af65e8

Drop support for ruby 1.9
Ruby 1.9 was broken by 3b279e031af01238bf30a46489a6fc418d90f21a

Ruby 1.9 reached EOL February 23, 2015, so I don't think it is worth
supporting.

.travis.yml
@@ -3,7 +3,6 @@ cache: bundler
 sudo: false
 before_install: gem update bundler
 rvm:
- - 1.9
  - 2.0.0
  - 2.1
  - 2.2

ffaker.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |s|
   s.version           = '2.7.0'
   s.date              = '2017-09-19'
   s.rubyforge_project = 'ffaker'
-  s.required_ruby_version = '>= 1.9'
+  s.required_ruby_version = '>= 2.0'
 
   s.license = 'MIT'
 