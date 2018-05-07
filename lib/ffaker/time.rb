b66af5f4cc2298402eb227da4f84afd49a3024db
Alexander Popov <alex.wayfer@gmail.com>
2018-05-08 01:59:08 +0300

    Return instances of classes corresponding to the names of `FFaker::Time` methods (#360)
    `FFaker::Time.between` returns instance of `Time`,
    so `.date` should returns instance of `Date` (without time),
    and `.datetime` should returns instance of `DateTime` (with time).
    
    Users can convert it to String as they want.

--- lib/ffaker/time.rb
+++ lib/ffaker/time.rb
@@ -18,28 +18,26 @@ module FFaker
       fetch_sample(DAYS_OF_WEEK)[0..(options[:long] ? 10 : 2)]
     end
 
-    def date(params = {})
+    def datetime(params = {})
       years_back = params[:year_range] || 5
       latest_year = params [:year_latest] || 0
-      year = (rand * years_back).ceil + (::Time.now.year - latest_year - years_back)
+      year = (rand * years_back).ceil + (::DateTime.now.year - latest_year - years_back)
       month = (rand * 12).ceil
       day = (rand * 31).ceil
-      hours = params[:hours] || 0
-      minutes = params[:minutes] || 0
-      series = [date = ::Time.local(year, month, day, hours, minutes)]
+      hours = params[:hours] || (rand * 12).ceil
+      minutes = params[:minutes] || (rand * 59).ceil
+      series = [date = ::DateTime.new(year, month, day, hours, minutes)]
       if params[:series]
         params[:series].each do |some_time_after|
           series << series.last + (rand * some_time_after).ceil
         end
         return series
       end
-      date.strftime '%Y-%m-%d %T %z'
+      date
     end
 
-    def datetime(params = {})
-      hours = params[:hours] || (rand * 12).ceil
-      minutes = params[:minutes] || (rand * 59).ceil
-      date(params.merge(hours: hours, minutes: minutes))
+    def date(params = {})
+      datetime(params).to_date
     end
 
     def between(from, to)
