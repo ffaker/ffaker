6c3f1d8b783a889244c1e2a2ef071a4086f5a353
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2018-05-10 19:55:51 +0000

    Implement FFaker::SSN.ssn according to the SSN validity rules

--- lib/ffaker/ssn.rb
+++ lib/ffaker/ssn.rb
@@ -11,7 +11,14 @@ module FFaker
     # http://en.wikipedia.org/wiki/Social_Security_number
     #
     def ssn
-      FFaker.numerify('###-##-####')
+      first_group  = [rand(1..665), rand(667..899)].sample
+      second_group = rand(1..99)
+      third_group  = rand(1..9999)
+
+      group_numbers = [first_group, second_group, third_group]
+      result = '%.3d-%.2d-%.4d' % group_numbers
+
+      result
     end
   end
 end
