5a4242779c1b226ce09a4de4164e1fd2d07facf3
Walerian <wsobczak@zendesk.com>
2018-09-29 11:32:24 +0200

    Add Polish genders.

---
+++ test/test_gender_pl.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'helper'
+
+class TestFakerGenderPL < Test::Unit::TestCase
+  include DeterministicHelper
+
+  GENDER_REGEX = /\A(kobieta|mężczyzna)\z/
+
+  assert_methods_are_deterministic(FFaker::GenderPL, :random)
+
+  def setup
+    @tester = FFaker::GenderPL
+  end
+
+  def test_random
+    assert_match(GENDER_REGEX, @tester.random)
+  end
+
+  def test_sample
+    assert_match(GENDER_REGEX, @tester.sample)
+  end
+
+  def test_maybe
+    assert_match(GENDER_REGEX, @tester.maybe)
+  end
+end
