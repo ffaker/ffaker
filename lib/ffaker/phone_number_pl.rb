b6ef6a34dd518dfb876718ebe33701ed67c20711
Walerian <wsobczak@zendesk.com>
2018-10-02 11:02:00 +0200

    Add Polish phone numbers.

---
+++ lib/ffaker/phone_number_pl.rb
@@ -0,0 +1,69 @@
+# encoding: utf-8
+
+module FFaker
+  # Author Walerian Sobczak <walerian.sobczak@gmail.com> github.com/walerian777
+  # Inspirations:
+  # https://pl.wikipedia.org/wiki/Numery_telefoniczne_w_Polsce
+  # https://pl.wikipedia.org/wiki/Plan_numeracji_krajowej
+  module PhoneNumberPL
+    extend ModuleUtils
+
+    module_function
+
+    PHONE_NUMBER_FORMAT = '%s#######'.freeze
+    COUNTRY_CODE = '+48'.freeze
+    MOBILE_PREFIXES = %w[50 51 53 57 60 66 69 72 73 78 79 88].freeze
+    AREA_CODES = %w[12 13 14 15 16 17 18 22 23 24 25 29 32 33
+                    34 41 42 43 44 46 48 52 54 55 56 58 59 61
+                    62 63 65 67 68 71 74 75 76 77 81 82 83 84
+                    85 86 87 89 91 94 95].freeze
+
+    def phone_number
+      case rand(0..1)
+      when 0 then home_work_phone_number
+      when 1 then mobile_phone_number
+      end
+    end
+
+    def home_work_phone_number
+      FFaker.numerify(phone_number_format)
+    end
+
+    def mobile_phone_number
+      FFaker.numerify(mobile_phone_number_format)
+    end
+
+    def international_phone_number
+      case rand(0..1)
+      when 0 then international_mobile_phone_number
+      when 1 then international_home_work_phone_number
+      end
+    end
+
+    def international_mobile_phone_number
+      FFaker.numerify("#{COUNTRY_CODE} #{mobile_phone_number_format}")
+    end
+
+    def international_home_work_phone_number
+      FFaker.numerify("#{COUNTRY_CODE} #{phone_number_format}")
+    end
+
+    def area_code
+      fetch_sample(AREA_CODES)
+    end
+
+    def mobile_prefix
+      fetch_sample(MOBILE_PREFIXES)
+    end
+
+    def phone_number_format
+      prefix = area_code
+      PHONE_NUMBER_FORMAT % prefix
+    end
+
+    def mobile_phone_number_format
+      prefix = mobile_prefix
+      PHONE_NUMBER_FORMAT % prefix
+    end
+  end
+end
---
+++ test/test_phone_number_pl.rb
@@ -0,0 +1,56 @@
+# encoding: utf-8
+
+require 'helper'
+
+class TestPhoneNumberPL < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::PhoneNumberPL,
+    :home_work_phone_number, :mobile_phone_number, :phone_number,
+    :area_code, :mobile_prefix,
+    :international_mobile_phone_number,
+    :international_home_work_phone_number,
+    :international_phone_number
+  )
+
+  def setup
+    @tester = FFaker::PhoneNumberPL
+  end
+
+  def test_home_work_phone_number
+    assert_match(/\A\d{9}\z/, @tester.home_work_phone_number)
+  end
+
+  def test_mobile_phone_number
+    assert_match(/\A\d{9}\z/, @tester.mobile_phone_number)
+  end
+
+  def test_phone_number
+    assert_match(/\A\d{9}\z/, @tester.phone_number)
+  end
+
+  def test_country_code
+    assert_equal(@tester::COUNTRY_CODE, '+48')
+  end
+
+  def test_international_mobile_phone_number
+    assert_match(/\A\+48 \d{9}\z/, @tester.international_mobile_phone_number)
+  end
+
+  def test_international_home_work_phone_number
+    assert_match(/\A\+48 \d{9}\z/, @tester.international_home_work_phone_number)
+  end
+
+  def test_international_phone_number
+    assert_match(/\A\+48 \d{9}\z/, @tester.international_phone_number)
+  end
+
+  def test_area_code
+    assert_include(@tester::AREA_CODES, @tester.area_code)
+  end
+
+  def test_mobile_prefixes
+    assert_include(@tester::MOBILE_PREFIXES, @tester.mobile_prefix)
+  end
+end
