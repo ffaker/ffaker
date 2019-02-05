1971c147df3a86ae01c046a9cb8b05145cc64ed7
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-02-05 19:14:51 +0300

    Fix RuboCop issues

--- test/test_healthcare_ru.rb
+++ test/test_healthcare_ru.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'helper'
 
 class TestHealthcareRU < Test::Unit::TestCase
   include DeterministicHelper
 
-  SPECIALIZATION_REGEX = /\A[А-Яа-я\ \(\)\-]+\z/.freeze
+  SPECIALIZATION_REGEX = /\A[А-Яа-я\ \(\)\-]+\z/
 
   assert_methods_are_deterministic(FFaker::HealthcareRU, :doctor_specialization)
 
