52e6ed759d1fac84468aad0e9d8e47526522bf4b
steverovsky <steverovsky@gmail.com>
2019-06-03 11:26:57 +0300

    fixed pattern in phone_number_ru UT

--- test/test_phone_number_ru.rb
+++ test/test_phone_number_ru.rb
@@ -19,42 +19,42 @@ class TestPhoneNumberRU < Test::Unit::TestCase
   end
 
   def test_international_phone_number
-    assert_match(/\+7 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A\+7 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.international_phone_number)
   end
 
   def test_international_mobile_phone_number
-    assert_match(/\+7 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A\+7 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.international_mobile_phone_number)
   end
 
   def test_international_home_work_phone_number
-    assert_match(/\+7 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A\+7 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.international_home_work_phone_number)
   end
 
   def test_international_toll_free_number
-    assert_match(/\+7 800 \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A\+7 800 \d{3}-\d{2}-\d{2}\z/,
                  @tester.international_toll_free_number)
   end
 
   def test_phone_number
-    assert_match(/8 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A8 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.mobile_phone_number)
   end
 
   def test_mobile_phone_number
-    assert_match(/8 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A8 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.mobile_phone_number)
   end
 
   def test_home_work_phone_number
-    assert_match(/8 \d{3} \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A8 \d{3} \d{3}-\d{2}-\d{2}\z/,
                  @tester.mobile_phone_number)
   end
 
   def test_toll_free_number
-    assert_match(/8 800 \d{3}-\d{2}-\d{2}/,
+    assert_match(/\A8 800 \d{3}-\d{2}-\d{2}\z/,
                  @tester.toll_free_number)
   end
 
