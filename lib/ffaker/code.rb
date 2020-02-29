c550fed33439dc7b91fb3eb9ade8accb14cc504f
Andy Libby <alibby@andylibby.org>
2020-02-28 23:27:31 -0500

    Add National Provider Identifier (npi) (#425)
    * Add National Provider Identifier (npi)
    
    * Check the whole string
    
    Co-authored-by: marocchino <marocchino@users.noreply.github.com>

--- lib/ffaker/code.rb
+++ lib/ffaker/code.rb
@@ -5,6 +5,23 @@ module FFaker
     extend ModuleUtils
     extend self
 
+    def npi(legacy: false)
+      max = legacy ? 299_999_999 : 999_999_999
+      base_npi = rand(100_000_000..max).to_s
+
+      summed_digits = base_npi
+        .chars
+        .each_with_index
+        .map { |n, i| (i.even? ? n.to_i * 2 : n).to_s }
+        .join
+        .chars
+        .inject(0) { |sum, digit| sum + digit.to_i }
+
+      npi_checksum = (10 - (24 + summed_digits) % 10).to_s.chars.last
+
+      base_npi + npi_checksum
+    end
+
     def ean
       ean = rand(100_000_000_000..999_999_999_999).to_s
 
--- test/test_code.rb
+++ test/test_code.rb
@@ -5,7 +5,28 @@ require 'helper'
 class TestCode < Test::Unit::TestCase
   include DeterministicHelper
 
-  assert_methods_are_deterministic(FFaker::Code, :ean)
+  assert_methods_are_deterministic(FFaker::Code, :ean, :npi)
+
+  def test_npi_legacy_regexp
+    assert FFaker::Code.npi(legacy: true).match(/\A(1|2)[0-9]{9}\z/)
+  end
+
+  def test_npi
+    FFaker::Random.seed = 523_456_789
+    assert FFaker::Code.npi == '7411850515'
+  end
+
+  def test_deterministic_npi
+    FFaker::Random.seed = 42
+    v = FFaker::Code.npi
+    FFaker::Random.reset!
+    assert v == FFaker::Code.npi
+  end
+
+  def test_valid_npi
+    FFaker::Random.seed = 123_456_789
+    assert FFaker::Code.npi == '2410167607'
+  end
 
   def test_ean
     assert FFaker::Code.ean.match(/^\d{13}$/)
