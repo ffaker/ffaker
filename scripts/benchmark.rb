32d8aa21ad760aa19041dfe6e583343348009e49
Voloshyn Vsevolod <seva.voloshin@gmail.com>
2020-03-10 10:09:10 +0200

    Improve benchmark file (#428)

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
-require 'rubygems'
 require 'benchmark'
 
-N = 10_000
+NAMES_COUNT = 10_000
 
 def run(name)
-  require name
   Benchmark.bm do |rep|
-    rep.report("generating #{N} names (#{name} #{FFaker::VERSION})") do
-      N.times do
-        FFaker::Name.name
-      end
+    rep.report("generating #{NAMES_COUNT} names (#{name})") do
+      mod == 'ffaker' ? FFaker : Faker
+      NAMES_COUNT.times { mod::Name.name }
     end
   end
-  $stdout.flush
-  exit(0)
 end
 
-fork { run('faker') }; Process.wait
-fork { run('ffaker') }; Process.wait
+['faker', 'ffaker'].each do |gem_name|
+  require gem_name
+  fork { run(gem_name) }; Process.wait
+end
