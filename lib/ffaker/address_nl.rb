2452013835d167259829105a1d0f805949be8450
Gerard <gerard@eet.nu>
2020-05-14 09:56:30 +0200

    Incorporated suggestions from @marocchino

--- lib/ffaker/address_nl.rb
+++ lib/ffaker/address_nl.rb
@@ -12,7 +12,7 @@ module FFaker
     POSTAL_CODE_FORMAT = ['#### ??'].freeze
 
     def postal_code
-      FFaker.bothify(POSTAL_CODE_FORMAT).tap { |p| p[0] = rand(1..9).to_s }
+      FFaker::String.from_regexp(/[1-9]\d{3} [A-Z]{2}/)
     end
 
     def zip_code
