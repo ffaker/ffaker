99ec17ce7f9e6e278648065508ba1fbf35189459
Hannes Maack <hannes@maack.io>
2021-02-09 10:09:34 +0100

    fixup! introduce  "Freedom Ipsum" text generator option (#271)

--- lib/ffaker/freedom_ipsum.rb
+++ lib/ffaker/freedom_ipsum.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'ffaker/lorem'
 
@@ -10,11 +10,11 @@ module FFaker
     extend self
 
     def word
-      FREEDOM_WORDS.sample
+      fetch_sample(FREEDOM_WORDS)
     end
 
     def words(num = 3)
-      FREEDOM_WORDS.sample(num)
+      fetch_sample(FREEDOM_WORDS, count: num)
     end
   end
 end
--- test/test_freedom_ipsum.rb
+++ test/test_freedom_ipsum.rb
@@ -1,23 +1,15 @@
+# frozen_string_literal: true
+
 require 'helper'
-
 class TestFreedomIpsum < Test::Unit::TestCase
-  TEST_REGEX = /\+1|[ a-z]+/i
+  include DeterministicHelper
 
-  def test_paragraph
-    assert_match(TEST_REGEX, FFaker::FreedomIpsum.paragraph)
-  end
+  assert_methods_are_deterministic(
+    FFaker::FreedomIpsum,
+    :word, :words
+  )
 
-  def test_sentence
-    assert_match(TEST_REGEX, FFaker::FreedomIpsum.sentence)
-  end
-
-  def test_paragraphs
-    assert_match(TEST_REGEX, FFaker::FreedomIpsum.paragraphs.join(' '))
-  end
-
-  def test_sentences
-    assert_match(TEST_REGEX, FFaker::FreedomIpsum.sentences.join(' '))
-  end
+  TEST_REGEX = /\+1|[ a-z]+/i.freeze
 
   def test_words
     assert_match(TEST_REGEX, FFaker::FreedomIpsum.words.join(' '))
