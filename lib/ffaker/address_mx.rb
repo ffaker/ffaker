37f8b79041efb6956dc789dec77ba41df47ed88a
Voloshyn Vsevolod <seva.voloshin@gmail.com>
2021-05-02 08:52:54 +0300

    Get rid of unneeded constant (#488)

--- lib/ffaker/address_mx.rb
+++ lib/ffaker/address_mx.rb
@@ -24,14 +24,8 @@ module FFaker
 
     def municipality(st_abbr = nil)
       st_abbr ||= state_abbr
-      MUNICIPALITY[st_abbr][rand(0...MUNICIPALITY[st_abbr].size)]
+      municipalities = Object.const_get("FFaker::AddressMX::#{st_abbr.upcase}")
+      fetch_sample(municipalities)
     end
-
-    MUNICIPALITY = { 'AGS' => AGS, 'BC' => BC, 'BCS' => BCS, 'CAMP' => CAMP, 'CHIS' => CHIS,
-                     'CHIH' => CHIH, 'COAH' => COAH, 'COL' => COL, 'DF' => DF, 'DGO' => DGO, 'GRO' => GRO,
-                     'GTO' => GTO, 'HGO' => HGO, 'JAL' => JAL, 'MEX' => MEX, 'MICH' => MICH, 'MOR' => MOR,
-                     'NL' => NL, 'NAY' => NAY, 'OAX' => OAX, 'PUE' => PUE, 'QR' => QR, 'QRO' => QRO, 'SLP' => SLP,
-                     'SIN' => SIN, 'SON' => SON, 'TAB' => TAB, 'TAMPS' => TAMPS, 'TLAX' => TLAX, 'VER' => VER,
-                     'YUC' => YUC, 'ZAC' => ZAC }.freeze
   end
 end
