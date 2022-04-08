74f264ceea08fbeb0437da71ae7e43b03eed5e6c
Gabriel Souza <61873782+az3vedo@users.noreply.github.com>
2022-04-08 13:36:27 -0300

    Add neighborhood and address complement to AddressBR (#512)
    * add complement sample file
    
    * add complement method
    
    * add complement test
    
    * add neighborhood_prefixes
    
    * add neighborhood method and its test

--- lib/ffaker/address_br.rb
+++ lib/ffaker/address_br.rb
@@ -37,9 +37,23 @@ module FFaker
       when 1 then "#{street_prefix} #{NameBR.first_name} #{NameBR.last_name} #{NameBR.last_name}"
       end
     end
-
-    def full_address
-      "#{street}, #{building_number}, #{city}, #{state}, Brazil"
+    
+    def complement
+      FFaker.numerify(fetch_sample(COMPLEMENT))
     end
+
+    def neighborhood
+      prefix = fetch_sample(NEIGHBORHOOD_PREFIXES)
+      "#{prefix} #{NameBR.first_name}"
+    end
+
+    def full_address(with_complement: false)
+      if with_complement
+        "#{street}, #{building_number}, #{complement}, #{neighborhood}, #{city}, #{state}, Brazil"
+      else
+      "#{street}, #{building_number}, #{neighborhood}, #{city}, #{state}, Brazil"
+      end
+    end
+
   end
 end
