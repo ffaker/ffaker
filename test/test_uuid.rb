847351e6d1f30ed622fbf0b3cf5c271bee0ddca3
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-10-30 12:03:08 -0700

    Add test to validate structure of UUID strings

--- test/test_uuid.rb
+++ test/test_uuid.rb
@@ -17,6 +17,8 @@ class TestUUID < Test::Unit::TestCase
   # @see https://stackoverflow.com/a/38191104
   def test_uuidv4
     raw_uuid = @tester.uuidv4
+    assert_format(raw_uuid)
+
     uuid = uuid_to_integer(raw_uuid)
     assert_version(uuid, 0b0100)
     assert_variant(uuid, 0b10)
@@ -24,6 +26,8 @@ class TestUUID < Test::Unit::TestCase
 
   def test_uuidv6
     raw_uuid = @tester.uuidv6
+    assert_format(raw_uuid)
+
     uuid = uuid_to_integer(raw_uuid)
     assert_version(uuid, 0b0110)
     assert_variant(uuid, 0b10)
@@ -31,6 +35,8 @@ class TestUUID < Test::Unit::TestCase
 
   def test_uuidv7
     raw_uuid = @tester.uuidv7
+    assert_format(raw_uuid)
+
     uuid = uuid_to_integer(raw_uuid)
     assert_version(uuid, 0b0111)
     assert_variant(uuid, 0b10)
@@ -38,12 +44,20 @@ class TestUUID < Test::Unit::TestCase
 
   def test_uuidv8
     raw_uuid = @tester.uuidv8
+    assert_format(raw_uuid)
+
     uuid = uuid_to_integer(raw_uuid)
     assert_version(uuid, 0b1000)
     assert_variant(uuid, 0b10)
   end
 
   private
+
+  # Matches structure of all UUID versions.
+  def assert_format(uuid)
+    assert_match(/\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/,
+                 uuid)
+  end
 
   def assert_version(uuid, version)
     assert_equal(version, (uuid >> 76) & 0b1111)
