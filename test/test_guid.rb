472a6ec9488c64e702e03971354a06866acc2950
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-10-30 12:02:37 -0700

    Update Guid test
    Handle UUID.uuidv4 returning lowercase; be strict about '4' in string.
    (In the UUID tests this the '4' is handled by assert_version.)

--- test/test_guid.rb
+++ test/test_guid.rb
@@ -8,7 +8,7 @@ class TestGuid < Test::Unit::TestCase
   assert_methods_are_deterministic(FFaker::Guid, :guid)
 
   def test_guid
-    assert_match(/[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}/,
+    assert_match(/\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/,
                  FFaker::Guid.guid)
   end
 end
