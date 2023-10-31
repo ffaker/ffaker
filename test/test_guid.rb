023ca67d6d0bcadb045053aa388d3e41bccf10d1
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-10-31 13:06:24 -0700

    Update tests for uppercase

--- test/test_guid.rb
+++ test/test_guid.rb
@@ -8,7 +8,7 @@ class TestGuid < Test::Unit::TestCase
   assert_methods_are_deterministic(FFaker::Guid, :guid)
 
   def test_guid
-    assert_match(/\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/,
+    assert_match(/\A[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}\z/,
                  FFaker::Guid.guid)
   end
 end
