61b79cc4be63beb1f92283030688ce8f1b23f735
Yen <130822984+cookiepingyen@users.noreply.github.com>
2025-08-28 15:04:55 +0800

    feat: add FFaker::LoremTW

---
+++ lib/ffaker/lorem_tw.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module FFaker
+  module LoremTW
+    extend ModuleUtils
+    extend self
+
+    def word
+      fetch_sample(WORDS)
+    end
+
+    def words(num = 3)
+      fetch_sample(WORDS, count: num)
+    end
+
+    def sentence(word_count = 4)
+      s = words(word_count + rand(0..5))
+      s = s.join
+      "#{s},"
+    end
+
+    def sentences(sentence_count = 3)
+      s = (1..sentence_count).map { sentence }
+      def s.to_s
+        result = join(' ')
+        result[-1] = '。'
+        result
+      end
+      s
+    end
+
+    def paragraph(sentence_count = 3)
+      sentences(sentence_count + rand(0..2)).to_s
+    end
+
+    def paragraphs(paragraph_count = 3)
+      (1..paragraph_count).map { paragraph }
+    end
+  end
+end
