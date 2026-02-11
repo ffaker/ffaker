59c6fd37a70dd1cf45c7395e77dd1b1c53fec047
Sam Partington <sam@goodbase.co.uk>
2026-02-11 11:05:41 +0000

    Make results deterministic

--- lib/ffaker/phone_number_gb.rb
+++ lib/ffaker/phone_number_gb.rb
@@ -7,7 +7,7 @@ module FFaker
 
     # Mobile prefixes are in the 071xx, 073xx, 074xx, 075xx, 07624, 077xx, 078xx and 079xx ranges
     def mobile_prefix(leading_zero = true)
-      mobile_prefix = %w[71 73 74 75 7624 77 78 79].sample
+      mobile_prefix = %w[71 73 74 75 7624 77 78 79][FFaker::Random.rand(0..7)]
       mobile_prefix += FFaker.numerify('##') unless mobile_prefix.length == 4
       mobile_prefix = "0#{mobile_prefix}" if leading_zero
       mobile_prefix
@@ -17,7 +17,7 @@ module FFaker
       # The region prefix affects the length and format of the subscriber number
       # so we just use a small subset of possible area codes here for simplicity
       # when generating the subscriber number.
-      region_prefix = %w[20 29 113 116 121 151 1382 1386 1865 1204 15396 16977].sample
+      region_prefix = %w[20 29 113 116 121 151 1382 1386 1865 1204 15396 16977][FFaker::Random.rand(0..11)]
       region_prefix = "0#{region_prefix}" if leading_zero
       region_prefix
     end
@@ -37,11 +37,11 @@ module FFaker
       subscriber_number = ''
       case region_for_comparison
       when '20'
-        subscriber_number = "#{%w[3 4 7 8].sample}#{FFaker.numerify('### ####')}"
+        subscriber_number = "#{%w[3 4 7 8][FFaker::Random.rand(0..3)]}#{FFaker.numerify('### ####')}"
       when '29'
         subscriber_number = "2#{FFaker.numerify('### ####')}"
       when '113', '116', '121', '151'
-        subscriber_number = "#{rand(0..4)}#{FFaker.numerify('## ####')}"
+        subscriber_number = "#{FFaker::Random.rand(0..4)}#{FFaker.numerify('## ####')}"
       when '1382', '1386', '1865', '1204'
         subscriber_number = FFaker.numerify('######')
       when '15396', '16977'
