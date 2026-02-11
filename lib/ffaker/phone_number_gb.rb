b7d50c25145c5225f8d2ee28eabe266d8b9ef7cd
Sam Partington <sam@goodbase.co.uk>
2026-02-11 10:48:51 +0000

    Add `PhoneNumberGB` for British phone numbers

---
+++ lib/ffaker/phone_number_gb.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module FFaker
+  module PhoneNumberGB
+    extend ModuleUtils
+    extend self
+
+    # Mobile prefixes are in the 071xx, 073xx, 074xx, 075xx, 07624, 077xx, 078xx and 079xx ranges
+    def mobile_prefix(leading_zero = true)
+      mobile_prefix = %w[71 73 74 75 7624 77 78 79].sample
+      mobile_prefix += FFaker.numerify('##') unless mobile_prefix.length == 4
+      mobile_prefix = "0#{mobile_prefix}" if leading_zero
+      mobile_prefix
+    end
+
+    def region_prefix(leading_zero = true)
+      # The region prefix affects the length and format of the subscriber number
+      # so we just use a small subset of possible area codes here for simplicity
+      # when generating the subscriber number.
+      region_prefix = %w[20 29 113 116 121 151 1382 1386 1865 1204 15396 16977].sample
+      region_prefix = "0#{region_prefix}" if leading_zero
+      region_prefix
+    end
+
+    def phone_number
+      home_work_phone_number
+    end
+
+    def mobile_phone_number(leading_zero = true)
+      [mobile_prefix(leading_zero), FFaker.numerify('#######')].join(' ')
+    end
+
+    def home_work_phone_number(leading_zero = true)
+      region = region_prefix(leading_zero)
+
+      region_for_comparison = region.delete_prefix('0')
+      subscriber_number = ''
+      case region_for_comparison
+      when '20'
+        subscriber_number = "#{%w[3 4 7 8].sample}#{FFaker.numerify('### ####')}"
+      when '29'
+        subscriber_number = "2#{FFaker.numerify('### ####')}"
+      when '113', '116', '121', '151'
+        subscriber_number = "#{rand(0..4)}#{FFaker.numerify('## ####')}"
+      when '1382', '1386', '1865', '1204'
+        subscriber_number = FFaker.numerify('######')
+      when '15396', '16977'
+        subscriber_number = FFaker.numerify('#####')
+      else
+        raise "Unexpected region prefix #{region_for_comparison}"
+      end
+
+      [region, subscriber_number].join(' ')
+    end
+
+    def country_code
+      '+44'
+    end
+
+    def international_mobile_phone_number
+      [country_code, mobile_phone_number(false)].join(' ')
+    end
+
+    def international_home_work_phone_number
+      [country_code, home_work_phone_number(false)].join(' ')
+    end
+
+    def international_phone_number
+      international_home_work_phone_number
+    end
+  end
+end
---
+++ test/test_phone_number_gb.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative 'helper'
+
+class TestPhoneNumberGB < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::PhoneNumberGB,
+    :mobile_prefix, :region_prefix, :mobile_phone_number, :home_work_phone_number,
+    :country_code, :international_mobile_phone_number, :international_home_work_phone_number
+  )
+
+  def setup
+    @tester = FFaker::PhoneNumberGB
+  end
+
+  def test_mobile_prefix
+    assert_match(/^07[0-9]{3}$/, FFaker::PhoneNumberGB.mobile_prefix)
+  end
+
+  def test_mobile_prefix_without_leading_zero
+    assert_match(/^7[0-9]{3}$/, FFaker::PhoneNumberGB.mobile_prefix(false))
+  end
+
+  def test_region_prefix
+    assert_match(/^0[1-9][0-9]{1,4}$/, FFaker::PhoneNumberGB.region_prefix)
+  end
+
+  def test_region_prefix_without_leading_zero
+    assert_match(/^[1-9][0-9]{1,4}$/, FFaker::PhoneNumberGB.region_prefix(false))
+  end
+
+  def test_mobile_phone_number
+    assert_match(/^07[0-9]{3} [0-9]{7}$/, FFaker::PhoneNumberGB.mobile_phone_number)
+  end
+
+  def test_home_work_phone_number
+    number = FFaker::PhoneNumberGB.home_work_phone_number
+    assert_match(/^0[1-9][0-9]{1,4} /, number) # starts with a region code
+    assert_match(/[0-9]{10}$/, number.gsub(/\s/, '')) # has 10 digits in total
+  end
+
+  def test_country_code
+    assert_match('+44', FFaker::PhoneNumberGB.country_code)
+  end
+
+  def test_international_mobile_phone_number
+    assert_match(/^\+44 7[0-9]{3} [0-9]{7}$/, FFaker::PhoneNumberGB.international_mobile_phone_number)
+  end
+
+  def test_international_home_work_phone_number
+    number = FFaker::PhoneNumberGB.international_home_work_phone_number
+    assert_match(/^\+44 [1-9][0-9]{1,4} /, number) # starts with country and region codes
+    assert_match(/^\+44[0-9]{10}$/, number.gsub(/\s/, '')) # remainder is 10 digits
+  end
+end
