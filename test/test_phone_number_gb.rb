3fe4dfea7da07dfc2233461c0b584ee5e7295e13
Sam Partington <sam@goodbase.co.uk>
2026-02-11 11:07:34 +0000

    Use @tester

--- test/test_phone_number_gb.rb
+++ test/test_phone_number_gb.rb
@@ -16,41 +16,41 @@ class TestPhoneNumberGB < Test::Unit::TestCase
   end
 
   def test_mobile_prefix
-    assert_match(/^07[0-9]{3}$/, FFaker::PhoneNumberGB.mobile_prefix)
+    assert_match(/^07[0-9]{3}$/, @tester.mobile_prefix)
   end
 
   def test_mobile_prefix_without_leading_zero
-    assert_match(/^7[0-9]{3}$/, FFaker::PhoneNumberGB.mobile_prefix(false))
+    assert_match(/^7[0-9]{3}$/, @tester.mobile_prefix(false))
   end
 
   def test_region_prefix
-    assert_match(/^0[1-9][0-9]{1,4}$/, FFaker::PhoneNumberGB.region_prefix)
+    assert_match(/^0[1-9][0-9]{1,4}$/, @tester.region_prefix)
   end
 
   def test_region_prefix_without_leading_zero
-    assert_match(/^[1-9][0-9]{1,4}$/, FFaker::PhoneNumberGB.region_prefix(false))
+    assert_match(/^[1-9][0-9]{1,4}$/, @tester.region_prefix(false))
   end
 
   def test_mobile_phone_number
-    assert_match(/^07[0-9]{3} [0-9]{7}$/, FFaker::PhoneNumberGB.mobile_phone_number)
+    assert_match(/^07[0-9]{3} [0-9]{7}$/, @tester.mobile_phone_number)
   end
 
   def test_home_work_phone_number
-    number = FFaker::PhoneNumberGB.home_work_phone_number
+    number = @tester.home_work_phone_number
     assert_match(/^0[1-9][0-9]{1,4} /, number) # starts with a region code
     assert_match(/[0-9]{10}$/, number.gsub(/\s/, '')) # has 10 digits in total
   end
 
   def test_country_code
-    assert_match('+44', FFaker::PhoneNumberGB.country_code)
+    assert_match('+44', @tester.country_code)
   end
 
   def test_international_mobile_phone_number
-    assert_match(/^\+44 7[0-9]{3} [0-9]{7}$/, FFaker::PhoneNumberGB.international_mobile_phone_number)
+    assert_match(/^\+44 7[0-9]{3} [0-9]{7}$/, @tester.international_mobile_phone_number)
   end
 
   def test_international_home_work_phone_number
-    number = FFaker::PhoneNumberGB.international_home_work_phone_number
+    number = @tester.international_home_work_phone_number
     assert_match(/^\+44 [1-9][0-9]{1,4} /, number) # starts with country and region codes
     assert_match(/^\+44[0-9]{10}$/, number.gsub(/\s/, '')) # remainder is 10 digits
   end
