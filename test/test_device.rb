91aaab7726608998a5bd154690b46a43424c29d1
MarcoAntonioMartins <44619636+MarcoAntonioMartins@users.noreply.github.com>
2026-03-12 05:30:29 -0300

    [FEAT] Add device module (#563)
    * Add FFaker::Device module with device names, manufacturers, platforms, and versioning methods
    
    * rubocop

---
+++ test/test_device.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'helper'
+
+class TestFakerDevice < Test::Unit::TestCase
+  include DeterministicHelper
+
+  assert_methods_are_deterministic(
+    FFaker::Device,
+    :name, :manufacturer, :platform, :version, :app_version
+  )
+
+  def test_name
+    assert_match(/\A[\w\s\-+]+\z/i, FFaker::Device.name)
+  end
+
+  def test_manufacturer
+    assert_match(/\A[\w\s]+\z/i, FFaker::Device.manufacturer)
+  end
+
+  def test_platform
+    assert_match(/\A[\w\s]+\z/i, FFaker::Device.platform)
+  end
+
+  def test_version
+    assert_match(/\A\d+\.\d+\z/, FFaker::Device.version)
+  end
+
+  def test_app_version
+    assert_match(/\A\d+\.\d+\.\d+\z/, FFaker::Device.app_version)
+  end
+end
